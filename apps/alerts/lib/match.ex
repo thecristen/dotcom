b85ff4da535c60a22c933637d284f6e27ed77451
Paul Swartz <pswartz@zipcar.com>
2016-06-09 15:45:46 -0400

    support matching alerts based on a time

--- apps/alerts/lib/match.ex
+++ apps/alerts/lib/match.ex
@@ -4,15 +4,40 @@ defmodule Alerts.Match do
   Returns the alerts which match the provided InformedEntity.
 
   """
+  use Timex
 
   alias Alerts.InformedEntity, as: IE
-  def match(alerts, entity) do
+  def match(alerts, entity, datetime \\ nil)
+  def match(alerts, entity, nil) do
     alerts
     |> Enum.filter(&(any_entity_match?(&1, entity)))
+  end
+  def match(alerts, entity, datetime) do
+    alerts
+    |> match(entity, nil)
+    |> Enum.filter(&(any_time_match?(&1, datetime)))
   end
 
   defp any_entity_match?(alert, entity) do
     alert.informed_entity
     |> Enum.any?(&(IE.match?(&1, entity)))
+  end
+
+  defp any_time_match?(alert, datetime) do
+    alert.active_period
+    |> Enum.any?(&(between?(&1, datetime)))
+  end
+
+  defp between?({start, nil}, datetime) do
+    datetime
+    |> Timex.after?(start)
+  end
+  defp between?({nil, stop}, datetime) do
+    datetime
+    |> Timex.before?(stop)
+  end
+  defp between?({start, stop}, datetime) do
+    datetime
+    |> Timex.between?(start, stop)
   end
 end
--- apps/alerts/test/match_test.exs
+++ apps/alerts/test/match_test.exs
@@ -1,5 +1,6 @@
 defmodule Alerts.MatchTest do
   use ExUnit.Case, async: true
+  use Timex
 
   alias Alerts.Alert
   alias Alerts.InformedEntity
@@ -50,5 +51,29 @@ defmodule Alerts.MatchTest do
     assert Alerts.Match.match(alerts, %InformedEntity{route_type: 1, route: "21"}) == []
     assert Alerts.Match.match(alerts, %InformedEntity{route_type: 1, route: "2", stop: "3"}) == alerts
     assert Alerts.Match.match(alerts, %InformedEntity{route_type: 1, route: "2", stop: "4"}) == alerts
+  end
+
+  test ".match can take a datetime to filter further" do
+    alerts = [
+      %Alert{
+        informed_entity: [
+          %InformedEntity{stop: "1"}
+        ],
+        active_period: [
+          {nil, DateTime.from({{2016, 6, 1}, {0, 0, 0}})},
+          {DateTime.from({{2016, 6, 2}, {0, 0, 0}}), DateTime.from({{2016, 6, 2}, {1, 0, 0}})},
+          {DateTime.from({{2016, 6, 3}, {0, 0, 0}}), nil}
+        ]
+      }
+    ]
+    ie = %InformedEntity{stop: "1"}
+
+    assert Alerts.Match.match(alerts, ie) == alerts
+    assert Alerts.Match.match(alerts, ie, DateTime.from({{2016, 6, 2}, {0, 30, 0}})) == alerts
+    assert Alerts.Match.match(alerts, ie, DateTime.from({{2016, 6, 4}, {0, 0, 0}})) == alerts
+    assert Alerts.Match.match(alerts, ie, DateTime.from({{2016, 5, 20}, {0, 0, 0}})) == alerts
+
+    assert Alerts.Match.match(alerts, ie, DateTime.from({{2016, 6, 1}, {12, 0, 0}})) == []
+    assert Alerts.Match.match(alerts, ie, DateTime.from({{2016, 6, 2}, {12, 0, 0}})) == []
   end
 end
