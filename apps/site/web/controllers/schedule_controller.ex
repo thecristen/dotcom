071d2e8e5f2e7551333f861fcefddcf7f3d66c61
Paul Swartz <pswartz@zipcar.com>
2016-06-09 10:33:14 -0400

    slowly improving schedule display

--- apps/site/web/controllers/schedule_controller.ex
+++ apps/site/web/controllers/schedule_controller.ex
@@ -2,7 +2,12 @@ defmodule Site.ScheduleController do
   use Site.Web, :controller
   use Timex
 
-  def index(conn, %{"route" => route}=params) do
+  def index(conn, %{"route" => route} = params) do
+    date = case Timex.parse(params["date"], "{ISOdate}") do
+             {:ok, value} -> value
+             _ -> Date.today
+           end
+
     direction_id = case params["direction_id"] do
                      nil -> 0
                      str -> String.to_integer(str)
@@ -10,13 +15,49 @@ defmodule Site.ScheduleController do
 
     schedules = Schedules.Repo.all(
       route: route,
-      date: Date.today,
+      date: date,
       direction_id: direction_id,
       stop_sequence: 1)
-    route = schedules
+
+    render(conn, "index.html",
+      date: date,
+      schedules: schedules,
+      direction: direction(direction_id),
+      route: route(schedules),
+      from: from(schedules),
+      to: to(schedules),
+      reverse_url: schedule_path(conn, :index,
+        route: route,
+        direction_id: rem(direction_id + 1, 2) |> Integer.to_string))
+  end
+
+  def direction(0), do: "Outbound"
+  def direction(1), do: "Inbound"
+  def direction(_), do: "Unknown"
+
+  def route(schedules) do
+    schedules
     |> List.first
     |> (fn schedule -> schedule.route end).()
+  end
 
-    render(conn, "index.html", schedules: schedules, route: route)
+  def from(schedules) do
+    schedules
+    |> Enum.map(fn schedule -> schedule.stop.name end)
+    |> most_frequent_value
+  end
+
+  def to(schedules) do
+    schedules
+    |> Enum.map(fn schedule -> schedule.trip.headsign end)
+    |> most_frequent_value
+  end
+
+  defp most_frequent_value(values) do
+    values
+    |> Enum.group_by(&(&1))
+    |> Enum.into([])
+    |> Enum.max_by(fn {_, items} -> length(items) end)
+    |> (fn {value, _} -> value end).()
   end
 end
--- apps/site/web/views/schedule_view.ex
+++ apps/site/web/views/schedule_view.ex
@@ -1,3 +1,15 @@
 defmodule Site.ScheduleView do
   use Site.Web, :view
+
+  def svg(_conn, path) do
+    svg_content = :site
+    |> Application.app_dir
+    |> Path.join("priv/static" <> path)
+    |> File.read!
+    |> String.split("\n")
+    |> Enum.drop(1) # drop the <?xml> header
+    |> Enum.join("")
+
+    raw svg_content
+  end
 end
