84e65ee3b3ddd95b991a91fa4f7e46675757dca2
Paul Swartz <pswartz@zipcar.com>
2016-06-09 10:55:43 -0400

    small code cleanups/date filter

--- apps/site/web/controllers/schedule_controller.ex
+++ apps/site/web/controllers/schedule_controller.ex
@@ -2,9 +2,9 @@ defmodule Site.ScheduleController do
   use Site.Web, :controller
   use Timex
 
-  def index(conn, %{"route" => route} = params) do
+  def index(conn, %{"route" => route_id} = params) do
     date = case Timex.parse(params["date"], "{ISOdate}") do
-             {:ok, value} -> value
+             {:ok, value} -> value |> Date.from
              _ -> Date.today
            end
 
@@ -14,31 +14,39 @@ defmodule Site.ScheduleController do
                    end
 
     all_schedules = Schedules.Repo.all(
-      route: route,
+      route: route_id,
       date: date,
       direction_id: direction_id,
       stop_sequence: 1)
-    show_all = params["all"] != nil
+
+    show_all = params["all"] != nil || not Timex.equal?(Date.today, date)
 
     render(conn, "index.html",
       date: date,
-      show_all_url: case show_all do
-                      false ->
-                        schedule_path(conn, :index,
-                          route: route,
-                          direction_id: direction_id |> Integer.to_string,
-                          all: "all")
-                      true ->
-                        nil
-                    end,
+      show_all_url: if show_all do
+        nil
+      else
+        update_url(conn, all: "all")
+      end,
       schedules: schedules(all_schedules, show_all),
       direction: direction(direction_id),
       route: route(all_schedules),
       from: from(all_schedules),
       to: to(all_schedules),
-      reverse_url: schedule_path(conn, :index,
-        route: route,
-        direction_id: rem(direction_id + 1, 2) |> Integer.to_string))
+      reverse_url: update_url(conn,
+        direction_id: reverse_direction(direction_id)))
+  end
+
+  def update_url(%{params: params} = conn, query) do
+    query_map = query
+    |> Enum.map(fn {key, value} -> {Atom.to_string(key), to_string(value)} end)
+    |> Enum.into(%{})
+
+    new_query = params
+    |> Map.merge(query_map)
+    |> Enum.into([])
+
+    schedule_path(conn, :index, new_query)
   end
 
   def schedules(all_schedules, true) do
@@ -50,26 +58,34 @@ defmodule Site.ScheduleController do
       Timex.after?(DateTime.now, time)
     end)
   end
+
   def direction(0), do: "Outbound"
   def direction(1), do: "Inbound"
   def direction(_), do: "Unknown"
 
-  def route(schedules) do
-    schedules
+  def route(all_schedules) do
+    all_schedules
     |> List.first
     |> (fn schedule -> schedule.route end).()
   end
 
-  def from(schedules) do
-    schedules
+  def from(all_schedules) do
+    all_schedules
     |> Enum.map(fn schedule -> schedule.stop.name end)
     |> most_frequent_value
   end
 
-  def to(schedules) do
-    schedules
+  def to(all_schedules) do
+    all_schedules
     |> Enum.map(fn schedule -> schedule.trip.headsign end)
     |> most_frequent_value
+  end
+
+  def reverse_direction(direction_id) do
+    direction_id
+    |> +(1)
+    |> rem(2)
+    |> Integer.to_string
   end
 
   defp most_frequent_value(values) do
