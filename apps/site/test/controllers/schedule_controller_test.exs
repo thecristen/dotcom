5b608f38b541146bef71e2a849532ae2eeae7a3a
Paul Swartz <pswartz@zipcar.com>
2016-06-10 08:06:35 -0400

    fix schedule controller test

--- apps/site/test/controllers/schedule_controller_test.exs
+++ apps/site/test/controllers/schedule_controller_test.exs
@@ -2,7 +2,9 @@ defmodule Site.ScheduleControllerTest do
   use Site.ConnCase
 
   test "lists all entries on index", %{conn: conn} do
-    conn = get conn, schedule_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing schedules"
+    conn = get conn, schedule_path(conn, :index, route: "CR-Lowell")
+    response = html_response(conn, 200)
+    assert response =~ "Lowell Line"
+    assert response =~ "North Station"
   end
 end
