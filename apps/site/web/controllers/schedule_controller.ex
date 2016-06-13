ebfeebabd7f0b0497e63a632ee2ade515d5d099a
Paul Swartz <pswartz@zipcar.com>
2016-06-13 15:29:24 -0400

    initial OD pair display

--- apps/site/web/controllers/schedule_controller.ex
+++ apps/site/web/controllers/schedule_controller.ex
@@ -2,19 +2,28 @@ defmodule Site.ScheduleController do
   use Site.Web, :controller
   use Timex
 
+  def index(conn, %{"origin" => origin_id, "dest" => dest_id} = params) do
+    default_params = index_params(params)
+    opts = []
+    |> Dict.put(:direction_id, default_direction_id(params))
+    |> Dict.put(:date, default_date(params))
+
+    pairs = Schedules.Repo.origin_destination(origin_id, dest_id, opts)
+    render(conn, "pairs.html", Keyword.merge(default_params, [
+              from: pairs |> List.first |> (fn {x, _} -> x.stop.name end).(),
+              to: pairs |> List.first |> (fn {_, y} -> y.stop.name end).(),
+              pairs: pairs
+            ]))
+  end
+
   def index(conn, %{"route" => route_id} = params) do
-    date = case Timex.parse(params["date"], "{ISOdate}") do
-             {:ok, value} -> value |> Date.from
-             _ -> Date.today
-           end
+    default_params = index_params(params)
 
-    direction_id = case params["direction_id"] do
-                     nil -> default_direction_id
-                     str -> String.to_integer(str)
-                   end
-
+    date = default_params[:date]
+    direction_id = default_params[:direction_id]
     trip = params
     |> Map.get("trip", "")
+
 
     basic_schedule_params = [
       route: route_id,
@@ -37,32 +46,59 @@ defmodule Site.ScheduleController do
       {Alerts.Repo, :all, []},
       {Site.ScheduleController, :selected_trip, [trip]}])
 
-    show_all = params["all"] != nil || not Timex.equal?(Date.today, date)
 
-    render(conn, "index.html",
-      date: date,
-      show_all: show_all,
-      schedules: schedules(all_schedules, show_all),
-      all_stops: all_stops,
-      direction: direction(direction_id),
-      reverse_direction: reverse_direction(direction_id),
-      route: route(all_schedules),
-      from: from(all_schedules),
-      to: to(all_schedules),
-      origin: params["origin"],
-      trip: trip,
-      trip_schedule: trip_schedule,
-      alerts: alerts)
+    render(conn, "index.html", Keyword.merge(default_params, [
+              all_stops: all_stops,
+              schedules: schedules(all_schedules, default_params[:show_all]),
+              route: route(all_schedules),
+              from: from(all_schedules),
+              to: to(all_schedules),
+              alerts: alerts,
+              trip: trip,
+              trip_schedule: trip_schedule
+            ]))
+
   end
 
-  def default_direction_id do
-    if DateTime.now("America/New_York").hour <= 13 do
-      1 # Inbound
-    else
-      0
+  def index_params(params) do
+    date = default_date(params)
+
+    direction_id = default_direction_id(params)
+
+    show_all = params["all"] != nil || not Timex.equal?(Date.today, date)
+
+    [
+      date: date,
+      show_all: show_all,
+      direction: direction(direction_id),
+      reverse_direction_id: reverse_direction_id(direction_id),
+      origin: params["origin"],
+      destination: params["dest"],
+    ]
+  end
+
+  def default_date(params) do
+    case Timex.parse(params["date"], "{ISOdate}") do
+      {:ok, value} -> value |> Date.from
+      _ -> Date.today
     end
   end
 
+  def default_direction_id(params) do
+    case params["direction_id"] do
+      nil ->
+        if DateTime.now("America/New_York").hour <= 13 do
+          1 # Inbound
+        else
+          0
+        end
+
+      str ->
+        String.to_integer(str)
+    end
+  end
+
+  def schedules(all_schedules, show_all)
   def schedules(all_schedules, true) do
     all_schedules
     |> sort_schedules
@@ -89,10 +125,8 @@ defmodule Site.ScheduleController do
   def direction(1), do: "Inbound"
   def direction(_), do: "Unknown"
 
-  def route(all_schedules) do
-    all_schedules
-    |> List.first
-    |> (fn schedule -> schedule.route end).()
+  def route([schedule|_]) do
+    schedule.route
   end
 
   def from(all_schedules) do
@@ -107,7 +141,7 @@ defmodule Site.ScheduleController do
     |> most_frequent_value
   end
 
-  def reverse_direction(direction_id) do
+  def reverse_direction_id(direction_id) do
     direction_id
     |> Kernel.+(1)
     |> rem(2)
