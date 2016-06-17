db06bb8bc5a0068abcb6dd93f0429a8e6b210748
Paul Swartz <pswartz@zipcar.com>
2016-06-17 10:43:52 -0400

    don't depend on 1.3

--- apps/repo_cache/mix.exs
+++ apps/repo_cache/mix.exs
@@ -8,7 +8,7 @@ defmodule RepoCache.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.3-rc",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
