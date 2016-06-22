9b90823825e0c93a0eaf124f62e9b3d64a012abd
Paul Swartz <pswartz@zipcar.com>
2016-06-22 14:12:56 -0400

    basic, hacky, green line overview

--- apps/site/web/controllers/schedule_controller.ex
+++ apps/site/web/controllers/schedule_controller.ex
@@ -33,6 +33,44 @@ defmodule Site.ScheduleController do
     )
   end
 
+  def index(conn, %{"route" => "Green"}) do
+    # special case for the Green Line summary
+    routes = for line <- ["B", "C", "D", "E"] do
+      %Schedules.Route{id: "Green-" <> line, name: line}
+    end
+
+    conn = conn
+    |> default_assigns
+    |> assign(:route, %Schedules.Route{id: 0, name: "Green", type: 0})
+    |> assign(:green_routes, routes)
+
+    conn
+    |> async_assign(:green_schedules, fn ->
+      routes
+      |> Enum.map(fn route ->
+        params = conn
+        |> assign(
+          :all_stops,
+          Schedules.Repo.stops(
+            route.id,
+           date: conn.assigns[:date],
+           direction_id: conn.assigns[:direction_id]))
+        |> basic_schedule_params
+
+        {route, params
+        |> Keyword.put(:route, route.id)
+        |> Schedules.Repo.all}
+      end)
+    end)
+    |> async_all_routes
+    |> await_assign_all
+    |> (fn(%{assigns: %{route: route, all_routes: all_routes}}=conn) ->
+      conn
+      |> assign(:all_routes, [route|all_routes])
+    end).()
+    |> render("green.html")
+  end
+
   def index(conn, %{"route" => route_id}) do
     conn = conn
     |> default_assigns
