42f1c21bd61e4372b663f7f8d7d91a40e3c6bf0b
Paul Swartz <pswartz@zipcar.com>
2016-06-23 13:04:09 -0400

    Revert "Redirect to the correct MBTA domain."
    This reverts commit 784c6fb18e20bbfaed3503253de3526d4d907995.
    
    http://mbta.com doesn't work on the internal network, but it is the
    correct domain since it handles the redirect to the mobile site.

--- apps/site/test/controllers/redirect_controller_test.exs
+++ apps/site/test/controllers/redirect_controller_test.exs
@@ -4,6 +4,6 @@ defmodule Site.RedirectControllerTest do
   test "shows chosen resource", %{conn: conn} do
     redirect = "schedules_and_maps/"
     conn = get conn, redirect_path(conn, :show, redirect)
-    assert html_response(conn, 200) =~ "http://www.mbta.com/schedules_and_maps/"
+    assert html_response(conn, 200) =~ "http://mbta.com/schedules_and_maps/"
   end
 end
