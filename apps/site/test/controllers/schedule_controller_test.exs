f84baf578f224c7ea9c73e6c22e93b5f8dea473c
Paul Swartz <pswartz@zipcar.com>
2016-06-27 14:00:11 -0400

    special case some default origins

--- apps/site/test/controllers/schedule_controller_test.exs
+++ apps/site/test/controllers/schedule_controller_test.exs
@@ -1,10 +1,27 @@
 defmodule Site.ScheduleControllerTest do
-  use Site.ConnCase
+  use Site.ConnCase, async: true
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, schedule_path(conn, :index, route: "CR-Lowell")
     response = html_response(conn, 200)
     assert response =~ "Lowell Line"
     assert response =~ "North Station"
+  end
+
+  test "inbound Lowell schedule contains the trip from Anderson/Woburn" do
+    conn = get conn, schedule_path(conn, :index, route: "CR-Lowell", all: "all", direction_id: 1)
+    response = html_response(conn, 200)
+    assert response =~ "from Anderson/ Woburn"
+  end
+
+  for route <- ["Red", "Blue", "Orange", "Green-B", "Green-C", "Green-D", "Green-E"] do
+    for direction_id <- [0, 1] do
+      name = "test_#{route}-#{direction_id} doesn't display 0 minute headway"
+      test name do
+        conn = get conn, schedule_path(conn, :index, route: unquote(route), all: "all", direction_id: unquote(direction_id))
+        response = html_response(conn, 200)
+        refute response =~ "Every 0-"
+      end
+    end
   end
 end
--- apps/site/web/controllers/schedule/query.ex
+++ apps/site/web/controllers/schedule/query.ex
@@ -1,22 +1,47 @@
 defmodule Site.ScheduleController.Query do
-  @doc """
+  @moduledoc """
   Given a conn, returns the relevant parameters for making a query
   of Schedules.Repo.all/1
   """
   def schedule_query(%Plug.Conn{params: params, assigns: assigns}) do
+    route = params["route"]
+    direction_id = assigns[:direction_id]
+
     schedule_params = [
-      route: params["route"],
+      route: route,
       date: assigns[:date],
-      direction_id: assigns[:direction_id]]
+      direction_id: direction_id]
 
-    stop_id = case Map.get(params, "origin", "") do
-                "" ->
-                  assigns[:all_stops]
-                  |> (fn [stop|_] -> stop.id end).()
-                value -> value
-              end
+    case Map.get(params, "origin", default_origin(route, direction_id)) do
+      "" ->
+        schedule_params
+        |> Keyword.put(:stop_sequence, 1)
 
-    schedule_params
-    |> Keyword.put(:stop, stop_id)
+     stop_id ->
+        schedule_params
+        |> Keyword.put(:stop, stop_id)
+
+    end
+  end
+
+  # special cases for the subways so that they pick up all relevant headways
+  # by default
+  defp default_origin("Red", 1) do
+    "70084" # Andrew
+  end
+  defp default_origin("Blue", 0) do
+    "70051" # Orient Heights - Inbound
+  end
+  defp default_origin("Orange", 0) do
+    "70036" # Oak Grove
+  end
+  defp default_origin("Green-D", 0) do
+    "70202" # Government Center
+  end
+  defp default_origin("Green-E", 0) do
+    "70202" # Government Center
+  end
+  defp default_origin(_, _) do
+    ""
   end
 end
