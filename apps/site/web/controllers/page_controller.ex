456be3d2d91b39f811de2e3a943c2f089e8c8697
Paul Swartz <pswartz@zipcar.com>
2016-06-29 12:44:29 -0400

    put a news image on the homepage

--- apps/site/web/controllers/page_controller.ex
+++ apps/site/web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
 defmodule Site.PageController do
   use Site.Web, :controller
+  import Phoenix.HTML, only: [raw: 1]
 
   def index(conn, _params) do
+    news = News.Repo.all(limit: 5)
     render conn, "index.html", %{
-      news: News.Repo.all(limit: 5),
+      news: news,
+      news_image: news_image(news),
       grouped_routes: grouped_routes
     }
   end
@@ -11,5 +14,15 @@ defmodule Site.PageController do
   defp grouped_routes do
     Routes.Repo.all
     |> Routes.Group.group
+  end
+
+  defp news_image(news) do
+    case news |> Enum.filter(&(&1.attributes["homepageicon"])) do
+      [entry|_] ->
+        raw entry.attributes["homepageicon"]
+        |> String.replace("src=\"/", "src=\"//www.mbta.com/")
+      _ ->
+        nil
+    end
   end
 end
