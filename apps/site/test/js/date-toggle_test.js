6fd35ed550a6b4cbefc36f07406656948d83d51a
Paul Swartz <pswartz@zipcar.com>
2016-07-05 14:24:10 -0400

    add missing test files

---
+++ apps/site/test/js/date-toggle_test.js
@@ -0,0 +1,37 @@
+import { assert } from 'chai';
+import jsdom from 'mocha-jsdom';
+import dateToggle from '../../web/static/js/date-toggle';
+
+describe('date-toggle', () => {
+  var $;
+  jsdom();
+
+  beforeEach(() => {
+    $ = require('jquery');
+    $('body').append('<div id=test/>');
+    $('#test').html(`
+<form>
+<span class='date-toggle'><input type=date></span>
+<span class='date-toggle date-toggle-enabled'>not input</span>
+<span class='date-toggle-edit'>edit</span>
+</form>
+`);
+  dateToggle($);
+  });
+
+  it('switches which element is enabled when edit is clicked', () => {
+    assert.lengthOf($('#test .date-toggle-enabled input'), 0);
+    $('#test .date-toggle-edit').click();
+    assert.lengthOf($('#test .date-toggle-enabled input'), 1);
+    $('#test .date-toggle-edit').click();
+    assert.lengthOf($('#test .date-toggle-enabled input'), 0);
+  });
+
+  it('focuses/clicks the date input', (done) => {
+    $('#test input').click(() => {
+      assert.isTrue($('#test input').is(':focus'));
+      done();
+    });
+    $('#test .date-toggle-edit').click();
+  });
+});
