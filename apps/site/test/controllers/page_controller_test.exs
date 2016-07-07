ebf98143aca4fafc45090253d9ed176f2c3019b4
Peter Fogg <peter@mbtace.com>
2016-07-07 14:31:06 -0400

    Testing CI changes.

--- apps/site/test/controllers/page_controller_test.exs
+++ apps/site/test/controllers/page_controller_test.exs
@@ -4,5 +4,6 @@ defmodule Site.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Massachusetts Bay Transportation Authority"
+    assert response_content_type(conn, :html) =~ "charset=utf-8"
   end
 end
