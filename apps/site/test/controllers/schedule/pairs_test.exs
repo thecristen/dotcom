205721fb558c5f439a57c89af4d092659b4ca138
Peter Fogg <peter@mbtace.com>
2016-07-12 10:01:01 -0400

    Add links to station pages from schedules.

--- apps/site/test/controllers/schedule/pairs_test.exs
+++ apps/site/test/controllers/schedule/pairs_test.exs
@@ -8,4 +8,11 @@ defmodule Site.ScheduleController.PairsTest do
     assert response =~ "Arrival"
     assert response =~ "Inbound to: North Station"
   end
+
+  test "links to origin and destination station pages", %{conn: conn} do
+    conn = get conn, schedule_path(conn, :index, origin: "place-alfcl", dest: "place-harsq", direction_id: "0")
+    response = html_response(conn, 200)
+    assert response =~ ~s(<a href="#{station_path(conn, :show, "place-alfcl")}">Alewife</a>)
+    assert response =~ ~s(<a href="#{station_path(conn, :show, "place-harsq")}">Harvard</a>)
+  end
 end
