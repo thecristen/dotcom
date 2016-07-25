882dd2294b907ddfb8a31ea5d53ef7db8b42c1af
Peter Fogg <peter@mbtace.com>
2016-07-25 15:43:35 -0400

    Bold nav links if active.

--- apps/site/web/views/layout_view.ex
+++ apps/site/web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule Site.LayoutView do
   use Site.Web, :view
+
+  def bold_if_active(conn, path, text) do
+    if conn.request_path == path do
+      "<strong>#{text}</strong>"
+    else
+      text
+    end
+    |> raw
+  end
 end
