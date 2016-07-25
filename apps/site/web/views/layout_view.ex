ceed67fa6a40fe964156f00c8532ae86e34eb648
Peter Fogg <peter@mbtace.com>
2016-07-25 16:35:26 -0400

    Bold prefixes on nav links, and add a test.

--- apps/site/web/views/layout_view.ex
+++ apps/site/web/views/layout_view.ex
@@ -2,7 +2,7 @@ defmodule Site.LayoutView do
   use Site.Web, :view
 
   def bold_if_active(conn, path, text) do
-    if conn.request_path == path do
+    if String.starts_with?(conn.request_path, path) do
       "<strong>#{text}</strong>"
     else
       text
