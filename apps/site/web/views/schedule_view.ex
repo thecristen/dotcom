2bb8f07c2d3ae8529557ae86b189501db7a17821
Peter Fogg <peter@mbtace.com>
2016-07-29 11:49:49 -0400

    Station information styling updates.
    - margin for content box
    - map padding
    - consistent styling for headings
    - consistent styling for route cells
    - less padding for mode group footers
    - less padding at the bottom of the page
    - backstop references

--- apps/site/web/views/schedule_view.ex
+++ apps/site/web/views/schedule_view.ex
@@ -126,11 +126,6 @@ defmodule Site.ScheduleView do
     |> raw
   end
 
-  def route_spacing_class(1), do: "col-xs-6 col-md-4"
-  def route_spacing_class(2), do: "col-xs-6 col-md-3"
-  def route_spacing_class(3), do: "col-xs-4 col-md-2"
-  def route_spacing_class(4), do: "col-xs-12 col-md-4"
-
   @doc "Link a station's name to its page, if it exists. Otherwise, just returns the name."
   def station_link(station) do
     case Stations.Repo.get(station.id) do
