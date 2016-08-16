e0e209a97c9f60830677a02824763679aa8866fb
Peter Fogg <peter@mbtace.com>
2016-08-16 10:20:26 -0400

    Allow disabling the usablenet link if a redirect is desktop-only.

--- apps/site/test/controllers/redirect_controller_test.exs
+++ apps/site/test/controllers/redirect_controller_test.exs
@@ -5,7 +5,15 @@ defmodule Site.RedirectControllerTest do
     redirect = "schedules_and_maps/"
     conn = get conn, redirect_path(conn, :show, redirect)
     response = html_response(conn, 200)
-    assert response =~ "http://www.mbta.com/schedules_and_maps/"
-    assert response =~ "http://mobile.usablenet.com/mt/www.mbta.com/schedules_and_maps/"
+    assert response =~ "http://www.mbta.com/" <> redirect
+    assert response =~ "http://mobile.usablenet.com/mt/www.mbta.com/" <> redirect
+  end
+
+  test "does not include the mobile link for t-alerts", %{conn: conn} do
+    redirect = "rider_tools/t_alerts/"
+    conn = get conn, redirect_path(conn, :show, redirect)
+    response = html_response(conn, 200)
+    assert response =~ "http://www.mbta.com/" <> redirect
+    refute response =~ "http://mobile.usablenet.com/mt/www.mbta.com/" <> redirect
   end
 end
