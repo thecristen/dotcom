54cb6a5465f0d28d47a905e49573e10c0869e5fb
Paul Swartz <pswartz@zipcar.com>
2016-09-01 14:46:27 -0400

    safer handling of direction_id

--- apps/site/web/controllers/schedule/defaults.ex
+++ apps/site/web/controllers/schedule/defaults.ex
@@ -44,17 +44,18 @@ defmodule Site.ScheduleController.Defaults do
     end
   end
 
-  defp default_direction_id(params) do
-    case params["direction_id"] do
-      nil ->
-        if now.hour <= 13 do
-          1 # Inbound
-        else
-          0
-        end
-
-      str ->
-        String.to_integer(str)
+  defp default_direction_id(%{"direction_id" => direction_str}) when is_binary(direction_str) do
+    case Integer.parse(direction_str) do
+      {0, _} -> 0
+      {1, _} -> 1
+      _ -> default_direction_id(nil) # fallback to the default
+    end
+  end
+  defp default_direction_id(_) do
+    if Util.now.hour <= 13 do
+      1 # Inbound
+    else
+      0
     end
   end
 
