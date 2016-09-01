cec7f29155ce31227bea98d14c6c12f48e8d9f36
Paul Swartz <pswartz@zipcar.com>
2016-09-01 14:56:52 -0400

    remove entity_for from Trip matcher

--- apps/alerts/lib/trip.ex
+++ apps/alerts/lib/trip.ex
@@ -16,40 +16,34 @@ defmodule Alerts.Trip do
   """
   def match(alerts, trip_ids, options \\ [])
   def match(alerts, trip_ids, options) when is_list(trip_ids) do
-    all_trip_entities = trip_ids
-    |> Enum.map(&(entity_for(&1, [])))
-    all_trip_alerts = Alerts.Match.match(
-      alerts,
-      all_trip_entities,
-      options[:time])
-
-    [all_trip_alerts,
+    [trip_alerts(alerts, trip_ids, options[:time]),
      delay_alerts(alerts, options)]
     |> Enum.concat
     |> Enum.uniq
   end
   def match(alerts, trip_id, options) do
-    [trip_alerts(alerts, trip_id, options[:time]),
-     delay_alerts(alerts, options)
-    ]
-    |> Enum.concat
-    |> Enum.uniq
+    match(alerts, [trip_id], options)
   end
 
-  defp trip_alerts(alerts, trip_id, time) do
+  defp trip_alerts(alerts, trip_ids, time) do
+    trip_entities = trip_ids
+    |> Enum.map(&(%Alerts.InformedEntity{trip: &1}))
+
     alerts
-    |> Alerts.Match.match(entity_for(trip_id, []), time)
+    |> Alerts.Match.match(trip_entities, time)
   end
 
   defp delay_alerts(alerts, options) do
+    entity = Alerts.InformedEntity.from_keywords(options)
+
     alerts
-    |> Alerts.Match.match(entity_for(nil, options), options[:time])
-    |> Enum.filter(&(&1.effect_name in ["Delay", "Suspension"]))
+    |> Enum.filter(&alert_is_delay?/1)
+    |> Alerts.Match.match(entity, options[:time])
   end
 
-  defp entity_for(trip_id, options) do
-    options
-    |> Keyword.put(:trip, trip_id)
-    |> Alerts.InformedEntity.from_keywords
-  end
+  defp alert_is_delay?(alert)
+  defp alert_is_delay?(%{effect_name: "Delay"}), do: true
+  defp alert_is_delay?(%{effect_name: "Suspension"}), do: true
+  defp alert_is_delay?(_), do: false
+
 end
