e72928f4b85960d17697142a8c4304922df183c6
Paul Swartz <paulswartz@gmail.com>
2016-09-09 12:22:25 -0400

    add origin/destination trip view
    asana: https://app.asana.com/0/111025076443530/172543723547213

--- apps/site/web/controllers/schedule/view_template.ex
+++ apps/site/web/controllers/schedule/view_template.ex
@@ -7,16 +7,16 @@ defmodule Site.ScheduleController.ViewTemplate do
                         schedules: schedules}} = conn, []) do
     conn
     |> assign(:view_template, view_template(schedules))
-    |> assign(:list_group_template, list_group_template(route_type))
+    |> assign(:list_group_template, list_group_template(schedules, route_type))
   end
 
   defp view_template([]), do: "empty.html"
-  defp view_template([{_, _} | _]), do: "pairs.html"
   defp view_template(_), do: "index.html"
 
-  defp list_group_template(0), do: "subway.html"
-  defp list_group_template(1), do: "subway.html"
-  defp list_group_template(2), do: "rail.html"
-  defp list_group_template(3), do: "bus.html"
-  defp list_group_template(4), do: "rail.html"
+  defp list_group_template([{_, _} | _], _), do: "pairs.html"
+  defp list_group_template(_, 0), do: "subway.html"
+  defp list_group_template(_, 1), do: "subway.html"
+  defp list_group_template(_, 2), do: "rail.html"
+  defp list_group_template(_, 3), do: "bus.html"
+  defp list_group_template(_, 4), do: "rail.html"
 end
