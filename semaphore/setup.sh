79e05a5bdc14adad2a2141d80f321f23d5d1f89e
Paul Swartz <paulswartz@gmail.com>
2016-09-16 11:34:27 -0400

    set GEM_SPEC so that gems are cached for builds

--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -1,10 +1,11 @@
 set -e
 
 mix local.hex --force
+mix local.rebar --force
 MIX_ENV=test mix do deps.get --only test, deps.compile
 nvm use 6.2
 rbenv local 2.3
-gem install sass pronto pronto-credo pronto-eslint pronto-scss
+GEM_SPEC=$SEMAPHORE_CACHE_DIR/gems gem install sass pronto pronto-credo pronto-eslint pronto-scss -N
 npm run install
 npm run brunch:build
 MIX_ENV=test mix compile --force
