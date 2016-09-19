b7baf74dc98dfcbd54853577ca8c73eb234963f2
Paul Swartz <paulswartz@gmail.com>
2016-09-19 13:43:35 -0400

    only include last N posts

--- apps/news/upload_to_s3.sh
+++ apps/news/upload_to_s3.sh
@@ -8,7 +8,10 @@
 # * MBTA_SQL_DATABASE
 # * S3_BUCKET
 
+# number of posts to include
+POST_COUNT=10
+
 ./import_posts.py priv/posts
 test -f posts.zip && rm -f posts.zip
-zip -9j posts.zip priv/posts/201{6,7,8,9}-*
+find priv/posts | sort | tail -n $POST_COUNT | xargs zip -9j posts.zip
 ../../aws/bin/aws s3 cp posts.zip s3://$S3_BUCKET/posts.zip --acl=public-read
