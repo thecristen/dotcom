188fd6183dadf071f7637e68d33d4358940c4e68
Kate Hedgpeth <kate@mbtace.com>
2016-09-19 18:38:03 -0400

    Cleaned up heading styles; consolidated Mode list views into one template

--- apps/site/web/views/mode_view.ex
+++ apps/site/web/views/mode_view.ex
@@ -1,3 +1,10 @@
 defmodule Site.ModeView do
   use Site.Web, :view
+
+  def get_route_group(:bus = route_type, route_groups, true = truncate) do
+    route_groups[route_type] |> Enum.filter(&(&1.key_route?))
+  end
+
+  def get_route_group(route_type, route_groups, _), do: route_groups[route_type]
+
 end
