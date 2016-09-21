e22882b9736fd295670b9686cdc30e50154ea6cf
Alex Sghia-Hughes <alex@mbtace.com>
2016-09-21 17:12:07 -0400

    consolidate showing full list functionality into one query param

--- apps/site/web/controllers/schedule/defaults.ex
+++ apps/site/web/controllers/schedule/defaults.ex
@@ -23,15 +23,12 @@ defmodule Site.ScheduleController.Defaults do
 
     show_all_schedules = params["all_schedules"] != nil || not Timex.equal?(today, date)
 
-    show_all_stops = params["all_stops"] != nil
-
-    show_all_times = params["all_times"] != nil
+    show_full_list = params["full_list"] != nil
 
     [
       date: date,
       show_all_schedules: show_all_schedules,
-      show_all_stops: show_all_stops,
-      show_all_times: show_all_times,
+      show_full_list: show_full_list,
       direction_id: direction_id,
       origin: case params["origin"] do
                 "" -> nil
