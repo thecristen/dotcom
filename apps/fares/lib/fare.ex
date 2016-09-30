10a8e7b25526b8a0338b0f4cc6bfb5d20aaa9645
Alex Sghia-Hughes <alex@mbtace.com>
2016-09-30 15:45:13 -0400

    typedocing

--- apps/fares/lib/fare.ex
+++ apps/fares/lib/fare.ex
@@ -1,11 +1,14 @@
 defmodule Fare do
   @typedoc """
-    name: :zone_1 | :interzone_4 | etc
-    pass_type: :ticket | :charlie_card | :mticket
-    reduced: :student | :senior_disabled
-    duration: :single_trip | :month
-    cents: <cost in cents>
+    struct(
+      name: :zone_1 | :interzone_4 | etc
+      pass_type: :ticket | :charlie_card | :mticket
+      reduced: :student | :senior_disabled
+      duration: :single_trip | :month
+      cents: cost_in_cents
+    )
   """
+  @type fare :: %Fare{name: :atom, pass_type: :atom, reduced: :atom, duration: :atom, cents: integer}
   defstruct [
     name: nil,
     pass_type: nil,
