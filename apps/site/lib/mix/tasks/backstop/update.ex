675f8fb97a082182311d1546a0ffa4a44139bf85
Peter Fogg <peter@mbtace.com>
2016-10-04 12:57:07 -0400

    Scripts for running/updating Backstop.

---
+++ apps/site/lib/mix/tasks/backstop/update.ex
@@ -0,0 +1,58 @@
+defmodule Mix.Tasks.Backstop.Update do
+  use Mix.Task
+
+  @shortdoc "Update Backstop references."
+  @moduledoc """
+  With no arguments, updates all failed references from the most recent test run. If arguments are given,
+  they should be a list of filenames to update.
+  """
+
+  @reference_dir "apps/site/backstop_data/bitmaps_test"
+
+  def run([]) do
+    latest_test_dir
+    |> latest_failures
+    |> Enum.each(&copy_file/1)
+  end
+  def run(filenames) do
+    test_dir = latest_test_dir
+
+    filenames
+    |> Enum.map(fn path ->
+      Path.expand(Path.join(test_dir, path))
+    end)
+    |> Enum.each(&copy_file/1)
+  end
+
+  def latest_test_dir do
+    @reference_dir
+    |> File.ls!
+    |> Enum.sort
+    |> Enum.map(&Path.join(@reference_dir, &1))
+    |> Enum.filter(&File.dir?/1)
+    |> List.last
+  end
+
+  def latest_failures(dir) do
+    dir
+    |> File.ls!
+    |> Enum.filter_map(&Kernel.=~(&1, "failed_diff_"), fn path ->
+      ~r/^failed_diff_(.*)$/
+      |> Regex.run(path, capture: :all_but_first)
+      |> List.first
+      |> (fn f -> Path.expand(Path.join(dir, f)) end).()
+    end)
+  end
+
+  def copy_file(path) do
+    filename = path
+    |> Path.split
+    |> List.last
+
+    destination = [@reference_dir, "../bitmaps_reference", filename]
+    |> Path.join
+    |> Path.expand
+
+    :ok = File.cp path, destination
+  end
+end
--- apps/site/mix.exs
+++ apps/site/mix.exs
@@ -69,7 +69,8 @@ defmodule Site.Mixfile do
      {:sizeable, "~> 0.1.5"},
      {:poison, "~> 2.2", override: true},
      {:feedback, in_umbrella: true},
-     {:laboratory, github: "paulswartz/laboratory", ref: "cookie_opts"}
+     {:laboratory, github: "paulswartz/laboratory", ref: "cookie_opts"},
+     {:porcelain, "~> 2.0"}
     ]
   end
 end
