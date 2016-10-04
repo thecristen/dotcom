a1be8991264155bf32ffcdcf3dcedaf09adc4431
Paul Swartz <paulswartz@gmail.com>
2016-10-04 16:19:22 -0400

    rename announcement container

--- apps/site/web/static/js/dismiss-announcement.js
+++ apps/site/web/static/js/dismiss-announcement.js
@@ -4,7 +4,7 @@ export default function($) {
   $(document).on('click', '#beta-announcement-dismiss', (event) => {
     const $target = $(event.currentTarget);
     event.preventDefault();
-    $target.parents(".alert-container").remove();
+    $target.parents(".announcement-container").remove();
     const cookieName = $target.data('cookie-name');
     document.cookie = `${cookieName}=true; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
   });
