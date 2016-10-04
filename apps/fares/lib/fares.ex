bbbd9d7e50dd5dae21d8a3a4135933a97fd12b44
Alex Sghia-Hughes <alex@mbtace.com>
2016-10-04 13:41:19 -0400

    calculate fare based on the origin/destination provided

--- apps/fares/lib/fares.ex
+++ apps/fares/lib/fares.ex
@@ -1,2 +1,28 @@
 defmodule Fares do
+
+  def calculate(start_zone, :zone_1a) do
+    Fares.Repo.all(name: start_zone)
+  end
+  def calculate(:zone_1a, end_zone) do
+    Fares.Repo.all(name: end_zone)
+  end
+  def calculate(start_zone, end_zone) do
+    zones = %{
+      zone_1: 1,
+      zone_2: 2,
+      zone_3: 3,
+      zone_4: 4,
+      zone_5: 5,
+      zone_6: 6,
+      zone_6: 7,
+      zone_6: 8,
+      zone_6: 9,
+      zone_6: 10
+    }
+
+    total_zones = abs(zones[start_zone] - zones[end_zone]) + 1 # need to include the starting zone
+
+    Fares.Repo.all(name: quote do: unquote(:"interzone_#{total_zones}"))
+  end
+
 end
--- apps/fares/test/fares_test.exs
+++ apps/fares/test/fares_test.exs
@@ -2,7 +2,42 @@ defmodule FaresTest do
   use ExUnit.Case
   doctest Fares
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "when the origin is zone 6, finds the zone 6 fares" do
+    assert Fares.calculate(:zone_6, :zone_1a) == [
+      %Fare{cents: 1000, duration: :single_trip, name: :zone_6,
+        pass_type: :ticket, reduced: nil},
+      %Fare{cents: 500, duration: :single_trip, name: :zone_6,
+        pass_type: :ticket, reduced: :student},
+      %Fare{cents: 500, duration: :single_trip, name: :zone_6,
+        pass_type: :ticket, reduced: :senior_disabled},
+      %Fare{cents: 31800, duration: :month, name: :zone_6,
+        pass_type: :ticket, reduced: nil}
+    ]
+  end
+
+  test "given two stops, finds the interzone fares" do
+    assert Fares.calculate(:zone_3, :zone_5) == [
+     %Fare{cents: 350, duration: :single_trip, name: :interzone_3,
+       pass_type: :ticket, reduced: nil},
+     %Fare{cents: 175, duration: :single_trip, name: :interzone_3,
+       pass_type: :ticket, reduced: :student},
+     %Fare{cents: 175, duration: :single_trip, name: :interzone_3,
+       pass_type: :ticket, reduced: :senior_disabled},
+     %Fare{cents: 11975, duration: :month, name: :interzone_3, pass_type: :ticket,
+       reduced: nil}
+    ]
+  end
+
+  test "when the origin is zone 1a, finds the fare based on destination" do
+    assert Fares.calculate(:zone_1a, :zone_4) == [
+      %Fare{cents: 825, duration: :single_trip, name: :zone_4, pass_type: :ticket,
+        reduced: nil},
+      %Fare{cents: 410, duration: :single_trip, name: :zone_4, pass_type: :ticket,
+        reduced: :student},
+      %Fare{cents: 410, duration: :single_trip, name: :zone_4, pass_type: :ticket,
+        reduced: :senior_disabled},
+      %Fare{cents: 26300, duration: :month, name: :zone_4, pass_type: :ticket,
+        reduced: nil}
+    ]
   end
 end
