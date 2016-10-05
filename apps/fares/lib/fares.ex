79728104f4408fac477420d87228f80c75d5131d
Alex Sghia-Hughes <alex@mbtace.com>
2016-10-05 10:03:23 -0400

    line length

--- apps/fares/lib/fares.ex
+++ apps/fares/lib/fares.ex
@@ -7,7 +7,8 @@ defmodule Fares do
     Fares.Repo.all(name: quote do: unquote(:"zone_#{end_zone}"))
   end
   def calculate(start_zone, end_zone) do
-    total_zones = abs(String.to_integer(start_zone) - String.to_integer(end_zone)) + 1 # need to include the starting zone
+    # we need to include all zones travelled in, ie zone 3 -> 5 is 3 zones
+    total_zones = abs(String.to_integer(start_zone) - String.to_integer(end_zone)) + 1
 
     Fares.Repo.all(name: quote do: unquote(:"interzone_#{total_zones}"))
   end
