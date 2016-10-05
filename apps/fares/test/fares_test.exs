093c8fc0bc4ae03a049522d222d5c98df9d73145
Alex Sghia-Hughes <alex@mbtace.com>
2016-10-05 13:42:13 -0400

    return an atom from calculate, which can be used to find the fare

--- apps/fares/test/fares_test.exs
+++ apps/fares/test/fares_test.exs
@@ -3,41 +3,14 @@ defmodule FaresTest do
   doctest Fares
 
   test "when the origin is zone 6, finds the zone 6 fares" do
-    assert Fares.calculate("6", "1A") == [
-      %Fare{cents: 1000, duration: :single_trip, name: :zone_6,
-        pass_type: :ticket, reduced: nil},
-      %Fare{cents: 500, duration: :single_trip, name: :zone_6,
-        pass_type: :ticket, reduced: :student},
-      %Fare{cents: 500, duration: :single_trip, name: :zone_6,
-        pass_type: :ticket, reduced: :senior_disabled},
-      %Fare{cents: 31_800, duration: :month, name: :zone_6,
-        pass_type: :ticket, reduced: nil}
-    ]
+    assert Fares.calculate("6", "1A") == :zone_6
   end
 
   test "given two stops, finds the interzone fares" do
-    assert Fares.calculate("3", "5") == [
-     %Fare{cents: 350, duration: :single_trip, name: :interzone_3,
-       pass_type: :ticket, reduced: nil},
-     %Fare{cents: 175, duration: :single_trip, name: :interzone_3,
-       pass_type: :ticket, reduced: :student},
-     %Fare{cents: 175, duration: :single_trip, name: :interzone_3,
-       pass_type: :ticket, reduced: :senior_disabled},
-     %Fare{cents: 11_975, duration: :month, name: :interzone_3, pass_type: :ticket,
-       reduced: nil}
-    ]
+    assert Fares.calculate("3", "5") == :interzone_3
   end
 
   test "when the origin is zone 1a, finds the fare based on destination" do
-    assert Fares.calculate("1A", "4") == [
-      %Fare{cents: 825, duration: :single_trip, name: :zone_4, pass_type: :ticket,
-        reduced: nil},
-      %Fare{cents: 410, duration: :single_trip, name: :zone_4, pass_type: :ticket,
-        reduced: :student},
-      %Fare{cents: 410, duration: :single_trip, name: :zone_4, pass_type: :ticket,
-        reduced: :senior_disabled},
-      %Fare{cents: 26_300, duration: :month, name: :zone_4, pass_type: :ticket,
-        reduced: nil}
-    ]
+    assert Fares.calculate("1A", "4") == :zone_4
   end
 end
