e0fdc376202deea392363cf0e80c25ee863a2f42
Alex Sghia-Hughes <alex@mbtace.com>
2016-10-05 13:26:24 -0400

    simplify atom creation in fares

--- apps/fares/lib/fares.ex
+++ apps/fares/lib/fares.ex
@@ -1,16 +1,16 @@
 defmodule Fares do
 
   def calculate(start_zone, "1A") do
-    Fares.Repo.all(name: quote do: unquote(:"zone_#{start_zone}"))
+    Fares.Repo.all(name: :"zone_#{start_zone}")
   end
   def calculate("1A", end_zone) do
-    Fares.Repo.all(name: quote do: unquote(:"zone_#{end_zone}"))
+    Fares.Repo.all(name: :"zone_#{end_zone}")
   end
   def calculate(start_zone, end_zone) do
     # we need to include all zones travelled in, ie zone 3 -> 5 is 3 zones
     total_zones = abs(String.to_integer(start_zone) - String.to_integer(end_zone)) + 1
 
-    Fares.Repo.all(name: quote do: unquote(:"interzone_#{total_zones}"))
+    Fares.Repo.all(name: :"interzone_#{total_zones}")
   end
 
 end
