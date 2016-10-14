8c3f087aded9c95376683014484ff13b517702a0
Alex Sghia-Hughes <alex@mbtace.com>
2016-10-14 11:47:04 -0400

    test organization

--- apps/site/test/views/fare_view_test.exs
+++ apps/site/test/views/fare_view_test.exs
@@ -13,43 +13,44 @@ defmodule Site.FareViewTest do
     assert FareView.fare_price(925) == "$9.25"
   end
 
-  test "fare duration is '1 Month' for a monthly fare" do
-    fare = %Fare{duration: :month}
-    assert FareView.fare_duration(fare.duration) == "1 Month"
+  describe "fare_duration/1" do
+    test "fare duration is '1 Month' for a monthly fare" do
+      assert FareView.fare_duration(:month) == "1 Month"
+    end
+
+     test "fare duration is 'Round Trip' for a round-trip fare" do
+       assert FareView.fare_duration(:round_trip) == "Round Trip"
+     end
+
+     test "fare duration is 'One Way' for a one-way fare" do
+       assert FareView.fare_duration(:single_trip) == "One Way"
+     end
   end
 
-   test "fare duration is 'Round Trip' for a round-trip fare" do
-     fare = %Fare{duration: :round_trip}
-     assert FareView.fare_duration(fare.duration) == "Round Trip"
-   end
+  describe "description/1" do
+    test "fare description for one way is for commuter rail only" do
+      fare = %Fare{duration: :single_trip}
+      assert FareView.description(fare) == "Valid for Commuter Rail only."
+    end
 
-   test "fare duration is 'One Way' for a one-way fare" do
-     fare = %Fare{duration: :single_trip}
-     assert FareView.fare_duration(fare.duration) == "One Way"
-   end
+    test "fare description for round trip is for commuter rail only" do
+      fare = %Fare{duration: :round_trip}
+      assert FareView.description(fare) == "Valid for Commuter Rail only."
+    end
 
-   test "fare description for one way is for commuter rail only" do
-     fare = %Fare{duration: :single_trip}
-     assert FareView.description(fare) == "Valid for Commuter Rail only."
-   end
+    test "fare description for month is describes the modes it can be used on" do
+      fare = %Fare{name: {:zone, "5"}, duration: :month}
 
-   test "fare description for round trip is for commuter rail only" do
-     fare = %Fare{duration: :round_trip}
-     assert FareView.description(fare) == "Valid for Commuter Rail only."
-   end
+      assert FareView.description(fare) ==
+        "Valid for one calendar month of unlimited travel on Commuter Rail from " <>
+        "Zones 1A-5 as well as Local Bus, Subway, Express Bus, and the Charlestown Ferry."
+    end
 
-   test "fare description for month is describes the modes it can be used on" do
-     fare = %Fare{name: {:zone, "5"}, duration: :month}
+    test "fare description for month mticket describes where it can be used" do
+      fare = %Fare{name: {:zone, "5"}, duration: :month, pass_type: :mticket}
 
-     assert FareView.description(fare) ==
-       "Valid for one calendar month of unlimited travel on Commuter Rail from " <>
-       "Zones 1A-5 as well as Local Bus, Subway, Express Bus, and the Charlestown Ferry."
-   end
-
-   test "fare description for month mticket describes where it can be used" do
-     fare = %Fare{name: {:zone, "5"}, duration: :month, pass_type: :mticket}
-
-     assert FareView.description(fare) ==
-       "Valid for one calendar month of travel on the commuter rail from zones 1A-5 only."
-   end
+      assert FareView.description(fare) ==
+        "Valid for one calendar month of travel on the commuter rail from zones 1A-5 only."
+    end
+  end
 end
