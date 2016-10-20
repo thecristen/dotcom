36870f18ea92713b2d8493e9d8b10c7da92d7ee0
Paul Swartz <paulswartz@gmail.com>
2016-10-20 17:33:23 -0400

    use new version of nodejs mirror envvar

--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -6,6 +6,6 @@ MIX_ENV=test mix do deps.get --only test, deps.compile
 nvm use 6.2
 rbenv local 2.3
 GEM_SPEC=$SEMAPHORE_CACHE_DIR/gems gem install sass pronto pronto-credo pronto-eslint pronto-scss -N
-npm run install
+NODEJS_ORG_MIRROR=$NVM_NODEJS_ORG_MIRROR npm run install
 npm run brunch:build
 MIX_ENV=test mix compile --force
