6c1c615bf0cb8c851cf5859ed41fa1388358166d
Paul Swartz <paulswartz@gmail.com>
2016-10-25 11:34:31 -0400

    updated text for adult single rides

--- apps/site/web/views/fare_view.ex
+++ apps/site/web/views/fare_view.ex
@@ -61,45 +61,36 @@ Bus, Subway, Express Bus, and the Charlestown Ferry."
  Subway, Express Buses, and Commuter Rail up to Zone 5."
     ]
   end
-  def description(%Fare{name: name, duration: :single_trip}) when name in [:inner_express_bus, :outer_express_bus] do
+  def description(%Fare{name: name, duration: :single_trip, pass_type: :cash_or_ticket})
+  when name in [:inner_express_bus, :outer_express_bus] do
     "No free or discounted transfers."
   end
   def description(%Fare{mode: :subway, pass_type: :charlie_card, duration: :single_trip} = fare) do
-    inner_express = [name: :inner_express_bus, pass_type: :charlie_card, duration: :single_trip]
-    |> Fares.Repo.all
-    |> List.first
-    outer_express = [name: :outer_express_bus, pass_type: :charlie_card, duration: :single_trip]
-    |> Fares.Repo.all
-    |> List.first
 
     [
-      "Free transfer to Subway and Local Bus. ",
-      "Transfer to Inner Express Bus ", Fares.Format.price(inner_express.cents - fare.cents), ". ",
-      "Transfer to Outer Express Bus ", Fares.Format.price(outer_express.cents - fare.cents), ". ",
+      "Valid for all Subway lines (includes routes SL1 and SL2). ",
+      transfers(fare),
       "Must be done within 2 hours of your original ride."
     ]
   end
-  def description(%Fare{mode: mode, pass_type: :cash_or_ticket}) when mode in [:subway, :bus] do
-    "Free transfer to Subway, route  SL4,  and route  SL5 when done within 2 hours of purchasing a ticket."
+  def description(%Fare{mode: :subway, pass_type: :cash_or_ticket}) do
+    "Free transfer to Subway, route SL4, and route SL5 when done within 2 hours of purchasing a ticket."
   end
   def description(%Fare{mode: :bus, pass_type: :charlie_card} = fare) do
-    subway = [name: :subway, pass_type: :charlie_card, duration: :single_trip]
-    |> Fares.Repo.all
-    |> List.first
-    inner_express = [name: :inner_express_bus, pass_type: :charlie_card, duration: :single_trip]
-    |> Fares.Repo.all
-    |> List.first
-    outer_express = [name: :outer_express_bus, pass_type: :charlie_card, duration: :single_trip]
-    |> Fares.Repo.all
-    |> List.first
-
     [
-      "Free transfer to Local Bus. ",
-      "Transfer to Subway ", Fares.Format.price(subway.cents - fare.cents), ". ",
-      "Transfer to Inner Express Bus ", Fares.Format.price(inner_express.cents - fare.cents), ". ",
-      "Transfer to Outer Express Bus ", Fares.Format.price(outer_express.cents - fare.cents), ". ",
+      "Valid for the Local Bus (includes route SL4 and SL5). ",
+      transfers(fare),
       "Must be done within 2 hours of your original ride."
     ]
+  end
+  def description(%Fare{mode: :bus, pass_type: :cash_or_ticket}) do
+    "Free transfer to one additional Local Bus included."
+  end
+  def description(%Fare{mode: :subway, duration: :month}) do
+    "Unlimited travel for one calendar month on the Local Bus and Subway."
+  end
+  def description(%Fare{mode: :subway, duration: duration}) when duration in [:day, :week] do
+    "Can be used for the Subway, Bus, Commuter Rail Zone 1A, and the Charlestown Ferry."
   end
   def description(%Fare{}) do
     "missing description"
@@ -159,5 +150,46 @@ are eligible for the reduced rate, however 1-Day, 7-Day, and Monthly Passes are 
 
   def update_fare_type(conn, reduced_type) do
     update_url(conn, fare_type: reduced_type)
+  end
+
+  def transfers(fare) do
+    # used to generate the list of transfer fees for a a given fare.  Filters out transfers which are <= 0.
+    {paid, free} = [subway: "Subway",
+                    local_bus: "Local Bus",
+                    inner_express_bus: "Inner Express Bus",
+                    outer_express_bus: "Outer Express Bus"]
+                    |> Enum.partition(&transfers_filter(&1, fare))
+    [
+      free_transfers(free),
+      Enum.map(paid, &transfers_map(&1, fare))
+    ]
+  end
+
+  defp transfers_filter({name, _}, fare) do
+    other_fare = transfers_other_fare(name)
+    other_fare.cents > fare.cents
+  end
+
+  defp free_transfers([]) do
+    []
+  end
+  defp free_transfers(names_and_texts) do
+    ["Free transfers to ",
+     names_and_texts
+     |> Enum.map(&elem(&1, 1))
+     |> AndJoin.join,
+     ". "
+    ]
+  end
+
+  defp transfers_map({name, text}, fare) do
+    other_fare = transfers_other_fare(name)
+    ["Transfer to ", text, " ", Fares.Format.price(other_fare.cents - fare.cents), ". "]
+  end
+
+  defp transfers_other_fare(name) do
+    [name: name, pass_type: :charlie_card, duration: :single_trip]
+    |> Fares.Repo.all
+    |> List.first
   end
 end
