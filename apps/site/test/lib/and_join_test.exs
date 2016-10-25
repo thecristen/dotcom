7425aefa0956f03713825600b969ae67d360f384
Paul Swartz <paulswartz@gmail.com>
2016-10-25 18:29:45 -0400

    use iodata_to_binary rather than custom function

--- apps/site/test/lib/and_join_test.exs
+++ apps/site/test/lib/and_join_test.exs
@@ -1,19 +1,13 @@
 defmodule AndJoinTest do
   use ExUnit.Case, async: true
   import AndJoin
+  import IO, only: [iodata_to_binary: 1]
 
   test "join/1" do
-    assert [] |> join |> collapse == ""
-    assert ["1"] |> join |> collapse == "1"
-    assert ["1", "2"] |> join |> collapse == "1 and 2"
-    assert ["1", "2", "3"] |> join |> collapse == "1, 2, and 3"
-    assert ["1", "2", "3", "4"] |> join |> collapse == "1, 2, 3, and 4"
-  end
-
-  defp collapse(string) when is_binary(string), do: string
-  defp collapse(list) when is_list(list) do
-    list
-    |> List.flatten
-    |> Enum.join("")
+    assert [] |> join |> iodate_to_binary == ""
+    assert ["1"] |> join |> iodate_to_binary == "1"
+    assert ["1", "2"] |> join |> iodate_to_binary == "1 and 2"
+    assert ["1", "2", "3"] |> join |> iodate_to_binary == "1, 2, and 3"
+    assert ["1", "2", "3", "4"] |> join |> iodate_to_binary == "1, 2, 3, and 4"
   end
 end
--- apps/site/test/views/fare/description_test.exs
+++ apps/site/test/views/fare/description_test.exs
@@ -2,6 +2,7 @@ defmodule Site.FareView.DescriptionTest do
   use ExUnit.Case, async: true
   import Site.FareView.Description
   alias Fares.Fare
+  import IO, only: [iodata_to_binary: 1]
 
   describe "description/1" do
     test "fare description for one way CR is for commuter rail only" do
@@ -17,7 +18,7 @@ defmodule Site.FareView.DescriptionTest do
     test "fare description for month is describes the modes it can be used on" do
       fare = %Fare{name: {:zone, "5"}, duration: :month, mode: :commuter}
 
-      assert fare |> description |> compact ==
+      assert fare |> description |> iodate_to_binary ==
         "Valid for one calendar month of unlimited travel on Commuter Rail from " <>
         "Zones 1A-5 as well as Local Bus, Subway, Express Bus, and the Charlestown Ferry."
     end
@@ -25,7 +26,7 @@ defmodule Site.FareView.DescriptionTest do
     test "fare description for month mticket describes where it can be used" do
       fare = %Fare{name: {:zone, "5"}, duration: :month, pass_type: :mticket, mode: :commuter}
 
-      assert fare |> description |> compact ==
+      assert fare |> description |> iodate_to_binary ==
         "Valid for one calendar month of travel on the commuter rail from Zones 1A-5 only."
     end
   end
@@ -33,7 +34,7 @@ defmodule Site.FareView.DescriptionTest do
   describe "transfers/1" do
     test "if current fare is cheapest, should have costs to other fares" do
       fare = %Fare{mode: :bus, name: :local_bus, duration: :single_trip, pass_type: :charlie_card, cents: 1}
-      result = fare |> transfers |> compact
+      result = fare |> transfers |> iodate_to_binary
       assert result =~ "Transfer to Subway $"
       assert result =~ "Transfer to Local Bus $"
       assert result =~ "Transfer to Inner Express Bus $"
@@ -42,12 +43,12 @@ defmodule Site.FareView.DescriptionTest do
 
     test "if current fare is most expensive, should have free transfers to other fares" do
       fare = %Fare{mode: :bus, name: :local_bus, duration: :single_trip, pass_type: :charlie_card, cents: 40000}
-      result = fare |> transfers |> compact
+      result = fare |> transfers |> iodate_to_binary
       expected = ["Free transfers to Subway",
                   "Local Bus",
                   "Inner Express Bus",
                   "Outer Express Bus."
-                 ] |> AndJoin.join |> compact
+                 ] |> AndJoin.join |> iodate_to_binary
       assert result == expected
     end
 
@@ -55,13 +56,10 @@ defmodule Site.FareView.DescriptionTest do
       inner_express_fare = Fares.Repo.all(name: :inner_express_bus, duration: :single_trip, pass_type: :charlie_card) |> List.first
       fare = %Fare{mode: :bus, name: :local_bus, duration: :single_trip, pass_type: :charlie_card, cents: inner_express_fare.cents - 1}
 
-      result = fare |> transfers |> compact
+      result = fare |> transfers |> iodate_to_binary
       assert result =~ "Transfer to Inner Express Bus $0.01."
       assert result =~ "Transfer to Outer Express Bus $1" # leave off cents
       assert result =~ "Free transfers to Subway and Local Bus."
     end
   end
-
-  def compact(string) when is_binary(string), do: string
-  def compact(list) when is_list(list), do: list |> List.flatten |> Enum.join("")
 end
