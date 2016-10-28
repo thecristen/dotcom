6f579db97bdd52e1a96cf537cca3823b3f69fe12
Alex Sghia-Hughes <alex@mbtace.com>
2016-10-28 16:37:58 -0400

    add brunch build to static analysis

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -5,3 +5,5 @@ export MIX_ENV=test
 pronto run -f github github_status -c origin/master || true
 # check for unsed variables and such
 mix compile --force --warnings-as-errors
+
+npm run brunch:build && test -f apps/site/priv/static/css/app.css && test -f apps/site/priv/static/js/app.js
