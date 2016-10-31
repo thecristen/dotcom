ac856a16b29d9e23ed6fe0f4d5487b1d33612266
Paul Swartz <paulswartz@gmail.com>
2016-10-31 09:37:05 -0400

    return a response if V3Api.get_json returns a non-200
    Stations.Api was expecting that behavior to handle Stations which are
    not Stops.

--- semaphore/smoke_test.sh
+++ semaphore/smoke_test.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+set -ex
 if [[ "$1" ]]; then
     DOMAIN=$1
+    shift
 else
     DOMAIN="localhost:4001"
 fi
-if time wget -D $DOMAIN --delete-after -nv -t 1 -e robots=off -r http://$DOMAIN/ -o - | grep --before-context=1 " ERROR "; then
+if time wget -D $DOMAIN --delete-after -nv -t 1 -e robots=off -r http://$DOMAIN/ -o - "$@"| grep --before-context=1 " ERROR "; then
     echo FAILED!
     exit 1
 else
