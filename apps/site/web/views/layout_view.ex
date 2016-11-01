50d07822de3e0f210a233301676f157ca1eac5a0
Peter Fogg <peter@mbtace.com>
2016-11-01 12:57:14 -0400

    Update desktop header.

--- apps/site/web/views/layout_view.ex
+++ apps/site/web/views/layout_view.ex
@@ -9,6 +9,13 @@ defmodule Site.LayoutView do
     end
   end
 
+  def format_header_fare(filters) do
+    filters
+    |> Fares.Repo.all
+    |> List.first
+    |> Fares.Format.price
+  end
+
   defp has_styleguide_subpages?(%{params: %{"section" => "content"}}), do: true
   defp has_styleguide_subpages?(%{params: %{"section" => "components"}}), do: true
   defp has_styleguide_subpages?(_), do: false
