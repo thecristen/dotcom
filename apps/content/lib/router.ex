c027f56c5b12219a9c97eb85bb0e8607c02648d8
Paul Swartz <paulswartz@gmail.com>
2016-11-09 12:35:07 -0500

    don't return all headers from static files
    In particular, the compression headers confused Chrome.

--- apps/content/lib/router.ex
+++ apps/content/lib/router.ex
@@ -8,15 +8,22 @@ defmodule Content.Router do
   plug :match
   plug :dispatch
 
+  @valid_resp_headers [
+    "content-type",
+    "date",
+    "etag",
+    "expires",
+    "last-modified",
+    "cache-control"
+  ]
+
   get "/sites/*_path" do
     full_url = Content.Config.url(conn.request_path)
     with {:ok, response} <- HTTPoison.get(full_url),
          %{status_code: 200} <- response do
       forward_response(conn, response)
     else
-      _ -> conn
-      |> halt
-      |> send_resp(:not_found, "")
+      _ -> send_resp(conn, :not_found, "")
     end
   end
 
@@ -29,9 +36,20 @@ defmodule Content.Router do
   defp forward_response(conn, %{body: body, headers: headers}) do
     headers
     |> Enum.reduce(conn, fn {key, value}, conn ->
-      put_resp_header(conn, key, value)
+      if String.downcase(key) in @valid_resp_headers do
+        put_resp_header(conn, header_case(key), value)
+      else
+        conn
+      end
     end)
     |> halt
     |> send_resp(:ok, body)
+  end
+
+  defp header_case(header) do
+    header
+    |> String.split("-")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join("-")
   end
 end
