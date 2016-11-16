dab548bc379c394e64db633ea736fc9615330c48
Paul Swartz <paulswartz@gmail.com>
2016-11-16 12:11:12 -0500

    use Haversine for distance

--- apps/stops/lib/distance.ex
+++ apps/stops/lib/distance.ex
@@ -7,11 +7,14 @@ defmodule Stops.Distance do
   alias Stops.Position
   import Stops.Position
 
+  @degrees_to_radians :math.pi / 180
+  @globe_diameter_km 6372.8 * 2
+
   @doc "Sorts the items by their distance from position."
   @spec sort([Position.t], Position.t) :: [Position.t]
   def sort(items, position) do
     items
-    |> Enum.sort_by(&distance_comp(position, &1))
+    |> Enum.sort_by(&haversine(position, &1))
   end
 
   @doc "Returns count items closest to position"
@@ -21,12 +24,29 @@ defmodule Stops.Distance do
   def closest(_, _, 0), do: []
   def closest(items, position, count) do
     items
-    |> Enum.sort_by(&distance_comp(position, &1))
+    |> Enum.sort_by(&haversine(position, &1))
     |> Enum.take(count)
   end
 
-  @doc "Return a value to compare the distance between points.  Does not represent an actual distance."
-  def distance_comp(first, second) do
-    :math.pow(latitude(first) - latitude(second), 2) + :math.pow(longitude(first) - longitude(second), 2)
+  @doc "Return the haversine distance between the two positions"
+  @spec haversine(Position.t, Position.t) :: float
+  def haversine(first, second) do
+    lat1 = latitude(first)
+    lat2 = latitude(second)
+    delta_lat = (lat1 - lat2) * @degrees_to_radians
+    delta_lon = (longitude(first) - longitude(second)) * @degrees_to_radians
+    lat1_radians = lat1 * @degrees_to_radians
+    lat2_radians = lat2 * @degrees_to_radians
+
+    @globe_diameter_km * :math.asin(
+      :math.sqrt(
+        sin2(delta_lat / 2) +
+        (:math.cos(lat1_radians) *
+          :math.cos(lat2_radians) *
+          sin2(delta_lon / 2))))
+  end
+
+  defp sin2(value) do
+    :math.pow(:math.sin(value), 2)
   end
 end
