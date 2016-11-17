d346a39967af02800c3ec1615e429b0a07a77bb8
Peter Fogg <peter@mbtace.com>
2016-11-17 11:44:53 -0500

    Open top-level menu items in new tab on cmd/ctrl-click.

---
+++ apps/site/web/static/js/menu-ctrl-click.js
@@ -0,0 +1,16 @@
+export default function($ = window.jQuery) {
+  $('.nav-link[data-parent="#desktop-menu"]').click(clickHandler($));
+};
+
+// Exported for testing
+export function clickHandler($) {
+  return function (event) {
+    if (event.ctrlKey || event.metaKey) { // ctrl for Windows, meta for others
+      const $this = $(this),
+            href = $(this).attr("href");
+      event.preventDefault();
+      event.stopPropagation();
+      window.open(href, "_blank");
+    }
+  };
+};
