02cd1009a9d8a8e9082e457b0b1d45cd8d1a10c3
Paul Swartz <paulswartz@gmail.com>
2016-12-02 12:07:19 -0500

    slightly less basic PC carousel

--- apps/site/web/static/js/carousel.js
+++ apps/site/web/static/js/carousel.js
@@ -3,15 +3,25 @@ export default function($) {
 
   function setupCarousel() {
     $(".carousel").each((_index, carousel) => {
-      $(carousel).on("click", ".carousel-item", (ev) => {
-        ev.preventDefault();
-        const $item = $(ev.currentTarget);
-        const $target = $(ev.delegateTarget.getAttribute("data-target"));
-        $target.html($item.html());
-      })
+      $(carousel)
+        .on("click", ".carousel-item", activateCarouselItem)
+        .on("keydown", ".carousel-item", (ev) => {
+          console.log(ev.keyCode);
+          if (ev.keyCode === 13 || ev.keyCode === 32) {
+            activateCarouselItem(ev);
+          }
+        })
         .find(".carousel-item:first-child")
         .click();
     });
   }
+
+  function activateCarouselItem(ev) {
+    ev.preventDefault();
+    const $item = $(ev.currentTarget);
+    const $target = $(ev.delegateTarget.getAttribute("data-target"));
+    $target.html($item.html());
+  }
+
   $(document).on('turbolinks:load', setupCarousel);
 };
