18f8734bd2cbc40241b2f4c3172ac200bd338730
Alex Sghia-Hughes <alex@mbtace.com>
2016-12-09 11:28:48 -0500

    first pass at service near me on the front page

--- apps/site/web/controllers/page_controller.ex
+++ apps/site/web/controllers/page_controller.ex
@@ -4,9 +4,25 @@ defmodule Site.PageController do
   plug Site.Plugs.Date
   plug Site.Plugs.Alerts
 
-  def index(conn, _params) do
+  def index(conn, %{"location" => %{"address" => address}}) do
+    results = address
+    |> GoogleMaps.Geocode.geocode
+    |> Site.ServiceNearMeController.get_stops_nearby(conn)
+    |> Site.ServiceNearMeController.stops_with_routes
+
     conn
     |> async_assign(:grouped_routes, &grouped_routes/0)
+    |> assign(:stops_with_routes, results)
+    |> assign(:address, address)
+    |> async_assign(:news, &news/0)
+    |> await_assign_all
+    |> render("index.html")
+  end
+  def index(conn, _) do
+    conn
+    |> async_assign(:grouped_routes, &grouped_routes/0)
+    |> assign(:stops_with_routes, [])
+    |> assign(:address, "")
     |> async_assign(:news, &news/0)
     |> await_assign_all
     |> render("index.html")
@@ -20,4 +36,9 @@ defmodule Site.PageController do
     Routes.Repo.all
     |> Routes.Group.group
   end
+
+  def address({:ok, [%{formatted: address} | _]}) do
+    address
+  end
+  def address(_), do: ""
 end
