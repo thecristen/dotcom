52b2246e9836afa40f0c2b812e037ff52440f9cd
kohlerjp <johnkohler@snow-mesa.local>
2016-12-09 12:27:29 -0500

    stop_schedule with commuter_rail

--- apps/site/web/views/stop_view.ex
+++ apps/site/web/views/stop_view.ex
@@ -187,12 +187,12 @@ defmodule Site.StopView do
   @spec upcoming_commuter_departures(Plug.Conn.t, String.t, String.t, integer) :: Schedules.Schedule.t | nil
   @doc "Returns the next departure for the given stop, CR line, and direction."
   def upcoming_commuter_departures(conn, stop, route, direction_id) do
-    stop
-    |> Schedules.Repo.schedule_for_stop(route: route, date: conn.assigns[:date], direction_id: direction_id)
+    conn.assigns.stop_schedule
+    |> route_schedule(route, direction_id)
     |> Enum.find(&(Timex.after?(&1.time, conn.assigns[:date_time]) && departing?(&1.trip.id, stop)))
   end
 
-  @spec upcoming_departures(%{date_time: DateTime.t, mode: Routes.Route.route_type, stop_schedule: [Schedules.Schedule.t], stop_predictions: [Predictions.Prediction.t]}, String.t, String.t, integer) :: [{:scheduled | :predicted, String.t, DateTime.t}]
+  @spec upcoming_departures(%{date_time: DateTime.t, mode: Routes.Route.route_type, stop_schedule: [Schedules.Schedule.t], stop_predictions: [Predictions.Prediction.t]}, String.t, integer, integer) :: [{:scheduled | :predicted, String.t, DateTime.t}]
   @doc "Returns the next departures for the given stop, route, and direction."
   def upcoming_departures(%{date_time: date_time, mode: mode, stop_schedule: stop_schedule, stop_predictions: stop_predictions}, stop_id, route_id, direction_id) do
     predicted =
