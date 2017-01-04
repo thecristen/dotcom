49a02281795ace7aa6425ffcbd6b36e09331f7ab
kohlerjp <john@mbtace.com>
2017-01-04 14:24:48 -0500

    upadte controller test

--- apps/site/test/controllers/schedule_v2/bus_controller_test.exs
+++ apps/site/test/controllers/schedule_v2/bus_controller_test.exs
@@ -4,7 +4,7 @@ defmodule Site.ScheduleV2.BusControllerTest do
   test "Contents of bus schedule template are rendered", %{conn: conn} do
     conn = get(conn, bus_path(conn, :show, "1"))
     response = html_response(conn, 200)
-    assert response =~ "Route"
+    assert response =~ "To view schedules for a specific"
   end
 
   test "renders a trip list from its origin", %{conn: conn} do
