de9ca3e67c716a17b375097f83a9da2a2a66884b
kohlerjp <john@mbtace.com>
2017-01-05 11:32:30 -0500

    add test for origin_destination

--- apps/site/test/controllers/schedule_v2/bus_controller_test.exs
+++ apps/site/test/controllers/schedule_v2/bus_controller_test.exs
@@ -12,4 +12,11 @@ defmodule Site.ScheduleV2.BusControllerTest do
     response = html_response(conn, 200)
     assert response =~ "Inbound to"
   end
+
+  test "renders a trip list with origin and destination", %{conn: conn} do
+    conn = get(conn, bus_path(conn, :origin_destination, "71", origin: "8178", dest: "2065"))
+    response = html_response(conn, 200)
+    assert response =~ "Departure"
+    assert response =~ "Arrival"
+  end
 end
