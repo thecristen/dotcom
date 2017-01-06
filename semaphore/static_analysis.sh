366eb21630a355a2dc6e9fcc028723d68143b5dc
Paul Swartz <paulswartz@gmail.com>
2017-01-06 16:38:58 -0500

    add dialyzer to static analysis

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -1,6 +1,8 @@
+#/usr/bin/env bash
 set -e
 
 export MIX_ENV=test
+export MIX_HOME=$SEMAPHORE_CACHE_DIR
 
 bash <(curl -s https://codecov.io/bash) -t $CODECOV_UPLOAD_TOKEN
 pronto run -f github github_status -c origin/master || true
@@ -8,3 +10,5 @@ pronto run -f github github_status -c origin/master || true
 mix compile --force --warnings-as-errors
 
 npm run brunch:build && test -f apps/site/priv/static/css/app.css && test -f apps/site/priv/static/js/app.js
+
+MIX_ENV=dev npm run dialyzer
