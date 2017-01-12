877719145f67fef7bb36a2ad30905885b1393b9b
Paul Swartz <paulswartz@gmail.com>
2017-01-12 10:19:55 -0500

    add banner to all pages if there's a banner alert

--- apps/alerts/lib/repo.ex
+++ apps/alerts/lib/repo.ex
@@ -1,6 +1,7 @@
 defmodule Alerts.Repo do
   use RepoCache, ttl: :timer.minutes(1)
 
+  @spec all() :: [Alerts.Alert.t]
   def all do
     cache nil, fn _ ->
       V3Api.Alerts.all().data
@@ -15,9 +16,24 @@ defmodule Alerts.Repo do
     end
   end
 
+  @spec by_id(String.t) :: Alerts.Alert.t | nil
   def by_id(id) do
     all()
     |> Enum.find(&(&1.id == id))
+  end
+
+  @spec banner() :: Alerts.Banner.t | nil
+  def banner() do
+    {:ok, result} = cache(&V3Api.Alerts.all/0, &do_banner/1)
+    result
+  end
+
+  @spec do_banner((() -> JsonApi.t)) :: {:ok, Alerts.Banner.t | nil}
+  def do_banner(alert_fn) do
+    result = alert_fn.().data
+      |> Enum.flat_map(&build_banner/1)
+      |> List.first
+    {:ok, result}
   end
 
   defp include_parents(alert) do
@@ -50,5 +66,23 @@ defmodule Alerts.Repo do
         _ -> [stop_id]
       end
     end)
+  end
+
+  @spec build_banner(JsonApi.Item.t) :: [Alerts.Banner.t]
+  defp build_banner(%JsonApi.Item{
+        id: id,
+        attributes: %{
+          "banner" => title,
+          "description" => description
+        }}) when title != nil do
+    [
+      %Alerts.Banner{
+        id: id,
+        title: title,
+        description: description}
+    ]
+  end
+  defp build_banner(%JsonApi.Item{}) do
+    []
   end
 end
