20abd50b1f548b281633075f8b7e4a6578538f68
Gene Shkolnik <gene@mbtace.com>
2017-01-17 17:07:35 -0500

    Small refactor; in schedule view show trips that are still en-route

--- apps/site/test/controllers/schedule_v2/offset_test.exs
+++ apps/site/test/controllers/schedule_v2/offset_test.exs
@@ -18,26 +18,79 @@ defmodule Site.ScheduleV2Controller.OffsetTest do
       assert conn.assigns.offset == 12
     end
 
-    test "without an offset explicitly provided, uses the index of the next departing schedule" do
+    test "when time is before the first trip offset is 0" do
       now = Util.now
 
-      conn = build_conn()
-      |> assign(:date_time, now)
-      |> assign(:all_schedules, Enum.map(-2..2, & %{time: Timex.shift(now, minutes: &1)}))
-      |> call([])
-
-      assert conn.assigns.offset == 3
-    end
-
-    test "if no schedules occur after now, uses 0 as the offset" do
-      now = Util.now
-
-      conn = build_conn()
-      |> assign(:date_time, now)
-      |> assign(:all_schedules, Enum.map(-5..-1, & %{time: Timex.shift(now, minutes: &1)}))
+      conn = now
+      |> make_timetable_schedules
+      |> assign(:date_time, Timex.shift(now, minutes: -1))
       |> call([])
 
       assert conn.assigns.offset == 0
     end
+
+    test "when time is during the first trip offset is 0" do
+      now = Util.now
+
+      conn = now
+      |> make_timetable_schedules
+      |> assign(:date_time, Timex.shift(now, minutes: 5))
+      |> call([])
+
+      assert conn.assigns.offset == 0
+    end
+
+    test "when time is right after the first trip offset is 1" do
+      now = Util.now
+
+      conn = now
+      |> make_timetable_schedules
+      |> assign(:date_time, Timex.shift(now, minutes: 21))
+      |> call([])
+
+      assert conn.assigns.offset == 1
+    end
+
+    test "when time is during the second trip offset is 1" do
+      now = Util.now
+
+      conn = now
+      |> make_timetable_schedules
+      |> assign(:date_time, Timex.shift(now, hours: 1, minutes: 5))
+      |> call([])
+
+      assert conn.assigns.offset == 1
+    end
+
+    test "when time is after the third trip offset is 0" do
+      now = Util.now
+
+      conn = now
+      |> make_timetable_schedules
+      |> assign(:date_time, Timex.shift(now, hours: 4))
+      |> call([])
+
+      assert conn.assigns.offset == 0
+    end
+  end
+
+  defp make_timetable_schedules(now) do
+    conn = build_conn()
+    |> assign(:date_time, now)
+    |> assign(:timetable_schedules, Enum.flat_map(0..2, &make_one_trip(&1, now)))
+  end
+
+  defp make_one_trip(i, now) do
+    Enum.map(0..2, 
+      &make_schedule(
+        Timex.shift(now, minutes: &1*10, hours: i), 
+        "trip" <> Integer.to_string(i), 
+        "stop" <> Integer.to_string(&1)))
+  end
+
+  defp make_schedule(time, trip_id, stop_id) do
+    %{time: time,
+      trip: %{id: trip_id},
+      stop: %{id: stop_id}}
   end
 end
