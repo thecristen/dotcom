aad20315cc45b259fcb6ef5e070b2affd22ef7ed
Alex Sghia-Hughes <alex@mbtace.com>
2017-01-19 11:08:56 -0500

    test assigns in controller instead of testing what was rendered

--- apps/site/test/controllers/schedule_v2_controller_test.exs
+++ apps/site/test/controllers/schedule_v2_controller_test.exs
@@ -2,49 +2,48 @@ defmodule Site.ScheduleV2ControllerTest do
   use Site.ConnCase, async: true
 
   describe "Bus and Subway:" do
-    test "Contents of schedule template are rendered", %{conn: conn} do
+    test "all stops is assigned for a route", %{conn: conn} do
       conn = get(conn, schedule_v2_path(conn, :show, "1"))
-      response = html_response(conn, 200)
-      assert response =~ "To view schedules for a specific"
+      html_response(conn, 200)
+      IO.inspect Map.keys(conn.assigns)
+      assert conn.assigns.all_stops != nil
     end
 
-    test "does not show a trip list until origin has been selected", %{conn: conn} do
-      conn = get(conn, schedule_v2_path(conn, :show, "1", direction_id: "1"))
-      response = html_response(conn, 200)
-      refute response =~ "Scheduled"
+    test "origin is unassigned for a route when you first view the page", %{conn: conn} do
+      conn = get(conn, schedule_v2_path(conn, :show, "1"))
+      html_response(conn, 200)
+      assert conn.assigns.origin == nil
     end
 
-    test "shows a trip list when origin has been selected", %{conn: conn} do
+    test "has the origin when it has been selected", %{conn: conn} do
       conn = get(conn, schedule_v2_path(conn, :show, "1", origin: "2167"))
-      response = html_response(conn, 200)
-      assert response =~ "Scheduled"
+      html_response(conn, 200)
+      assert conn.assigns.origin == "2167"
     end
 
-    test "renders a trip list with origin and destination", %{conn: conn} do
+    test "finds a trip when origin has been selected", %{conn: conn} do
+      conn = get(conn, schedule_v2_path(conn, :show, "1", origin: "2167"))
+      html_response(conn, 200)
+      assert conn.assigns.origin == "2167"
+      assert conn.assigns.trip != nil
+    end
+
+    test "finds a trip list with origin and destination", %{conn: conn} do
       conn = get(conn, schedule_v2_path(conn, :show, "71", origin: "8178", dest: "2065"))
-      response = html_response(conn, 200)
-      assert response =~ "Departure"
-      assert response =~ "Arrival"
-    end
-
-    test "destination selector appears when origin is selected", %{conn: conn} do
-      conn = get(conn, schedule_v2_path(conn, :show, "71", origin: "8178"))
-      response = html_response(conn, 200)
-      assert response =~ "Arriving at:"
-    end
-
-    test "destination selector is not visible when no origin has been selected", %{conn: conn} do
-      conn = get(conn, schedule_v2_path(conn, :show, "60"))
-      response = html_response(conn, 200)
-      refute response =~ "Arriving at:"
+      html_response(conn, 200)
+      assert conn.assigns.origin == "8178"
+      assert conn.assigns.destination == "2065"
+      assert conn.assigns.trip != nil
+      assert conn.assigns.schedules != nil
+      assert conn.assigns.predictions != nil
     end
   end
 
   describe "Subway only:" do
-    test "Renders frequency of transit on a line", %{conn: conn} do
+    test "has the full list of schedules so in order to calculate frequencys for all time blocks", %{conn: conn} do
       conn = get(conn, schedule_v2_path(conn, :frequency, "11"))
-      response = html_response(conn, 200)
-      assert response =~ "Schedules For Today"
+      html_response(conn, 200)
+      assert conn.assigns.all_schedules != nil
     end
   end
 end
