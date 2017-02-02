a5749aa6441f578452245cf71d9404b790d6ce1a
Paul Swartz <paulswartz@gmail.com>
2017-02-02 15:18:29 -0500

    update some cache directories

--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -15,9 +15,12 @@ mix local.rebar --force
 MIX_ENV=test mix do deps.get, deps.compile
 nvm use 6.2
 rbenv local 2.4
+mkdir -p $SEMAPHORE_CACHE_DIR/gems $SEMAPHORE_CACHE_DIR/npm
 GEM_SPEC=$SEMAPHORE_CACHE_DIR/gems gem install -g gem.deps.rb sass pronto pronto-credo pronto-eslint pronto-scss -N
 # drop phantomjs/backstop/casper from the deps to install
 sed -r -e 's/.*"(phantomjs-prebuilt|backstopjs|casperjs)".*//' -i'' apps/site/package.json
+# set cache dir for node
+npm config set cache $SEMAPHORE_CACHE_DIR/npm
 NODEJS_ORG_MIRROR=$NVM_NODEJS_ORG_MIRROR npm run install --no-optional
 npm run brunch:build
 MIX_ENV=test mix compile --force
