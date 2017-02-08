26b2ce49e0f3663e72522fbab05f442cc902fdc7
Peter Fogg <peter@mbtace.com>
2017-02-08 13:12:37 -0500

    Update excluded stops logic to work for the Green Line.
    I also added a direction_id parameter to GreenLine.stops_on_routes for
    testing purposes (and because it will be needed on the GL schedules
    page). This necessitated some changes to the filtering logic so that
    we get the correct stops in either direction.

--- apps/site/lib/green_line.ex
+++ apps/site/lib/green_line.ex
@@ -6,17 +6,18 @@ defmodule GreenLine do
   alias Routes.Route
   alias Stops.Stop
 
-  @type stop_routes_pair :: {[Stop.t], MapSet.t}
+  @type route_id_stop_id_map :: %{Route.id_t => MapSet.t}
+  @type stop_routes_pair :: {[Stop.t], route_id_stop_id_map}
 
   @doc """
   Returns the list of Green Line stops, as well as a MapSet of {stop_id, route_id} pairs to signify
   that a stop is on the branch in question.
   """
-  @spec stops_on_routes() :: stop_routes_pair
-  def stops_on_routes() do
+  @spec stops_on_routes(0 | 1) :: stop_routes_pair
+  def stops_on_routes(direction_id) do
     ~w(Green-B Green-C Green-D Green-E)s
-    |> Task.async_stream(&green_line_stops/1)
-    |> Enum.reduce({[], MapSet.new}, &merge_green_line_stops/2)
+    |> Task.async_stream(&green_line_stops(&1, direction_id))
+    |> Enum.reduce({[], %{}}, &merge_green_line_stops/2)
   end
 
   @doc """
@@ -30,32 +31,87 @@ defmodule GreenLine do
   def terminus?(stop_id, "Green-E") when stop_id in ["place-lech", "place-hsmnl"], do: true
   def terminus?(_, _), do: false
 
+  @doc """
+  Given a stop ID, route ID, and route => stop set map, returns whether the stop is on the route.
+  """
+  @spec stop_on_route?(Stop.id_t, Route.id_t, stop_routes_pair) :: boolean
+  def stop_on_route?(stop_id, route_id, {_, map}) do
+    MapSet.member?(map[route_id], stop_id)
+  end
+
+  @doc """
+  Returns all the stops on Green Line.
+  """
+  @spec all_stops(stop_routes_pair) :: [Stop.t]
+  def all_stops({stops, _}) do
+    stops
+  end
+
+  @doc """
+  Returns stops on the specific branch of the line.
+  """
+  @spec route_stops(Route.id_t, stop_routes_pair) :: MapSet.t
+  def route_stops(route_id, {_, map}) do
+    map[route_id]
+  end
+
   # Returns the stops that are on a given branch of the Green line,
   # along with the route ID.
-  @spec green_line_stops(Route.id_t) :: {Route.id_t, [Stop.t]}
-  defp green_line_stops(route_id) do
+  @spec green_line_stops(Route.id_t, 0 | 1) :: {Route.id_t, [Stop.t]}
+  defp green_line_stops(route_id, direction_id) do
     stops = route_id
-    |> Stops.Repo.by_route(0)
-    |> Enum.drop_while(& not terminus?(&1.id, route_id))
+    |> Stops.Repo.by_route(direction_id)
+    |> filter_lines(route_id)
 
     {route_id, stops}
   end
 
+  @spec filter_lines([Stop.t], Route.id_t) :: [Stop.t]
+  defp filter_lines(stops, route_id) do
+    stops
+    |> do_filter_lines(route_id, false, [])
+    |> Enum.reverse
+  end
+
+  # Basically a state machine -- when one terminal is encountered it
+  # begins adding stops to the accumulator; it then proceeds down the
+  # list of stops until the other terminal is seen, at which point it
+  # adds it on and returns the full list.
+  @spec do_filter_lines([Stop.t], Route.id_t, boolean, [Stop.t]) :: [Stop.t]
+  defp do_filter_lines(stops, route_id, in_line?, acc)
+  defp do_filter_lines([], _route_id, _in_line?, acc) do
+    acc
+  end
+  defp do_filter_lines([stop | stops], route_id, false, []) do
+    if terminus?(stop.id, route_id) do
+      do_filter_lines(stops, route_id, true, [stop])
+    else
+      do_filter_lines(stops, route_id, false, [])
+    end
+  end
+  defp do_filter_lines([stop | stops], route_id, true, acc) do
+    if terminus?(stop.id, route_id) do
+      [stop | acc]
+    else
+      do_filter_lines(stops, route_id, true, [stop | acc])
+    end
+  end
+
   # Returns the current full list of stops on the Green line, along with a
-  # MapSet for all {stop_id, route_id} pairs where that stop in on that route.
+  # map of {route_id => [stop_id]} representing all the stops on the route.
   # The {:ok, _} part of the pattern match is due to using Task.async_stream.
   @spec merge_green_line_stops({:ok, {Route.id_t, [Stop.t]}}, stop_routes_pair) :: stop_routes_pair
-  defp merge_green_line_stops({:ok, {route_id, line_stops}}, {current_stops, stop_route_id_set}) do
-    # update stop_route_id_set to tag the routes the stop is on
-    stop_route_id_set = line_stops
-    |> Enum.reduce(stop_route_id_set, fn %{id: stop_id}, set ->
-      MapSet.put(set, {stop_id, route_id})
+  defp merge_green_line_stops({:ok, {route_id, line_stops}}, {current_stops, route_id_stop_map}) do
+    # Update route_id_stop_map to include the stop
+    route_id_stop_map = line_stops
+    |> Enum.reduce(route_id_stop_map, fn %{id: stop_id}, map ->
+      Map.put(map, route_id, MapSet.put(Map.get(map, route_id, MapSet.new), stop_id))
     end)
 
     current_stops = line_stops
     |> List.myers_difference(current_stops)
     |> Enum.flat_map(fn {_op, stops} -> stops end)
 
-    {current_stops, stop_route_id_set}
+    {current_stops, route_id_stop_map}
   end
 end
--- apps/site/test/excluded_stops_test.exs
+++ apps/site/test/excluded_stops_test.exs
@@ -3,7 +3,7 @@ defmodule ExcludedStopsTest do
   import ExcludedStops
 
   describe "excluded_origin_stops/3" do
-    test "excludes the last stop on a non-Red line" do
+    test "excludes the last stop on a non-Red or Green line" do
       all_stops = Enum.map(1..10, & %{id: Integer.to_string(&1)})
 
       assert excluded_origin_stops(0, "Route", all_stops) == ["10"]
@@ -22,10 +22,22 @@ defmodule ExcludedStopsTest do
     test "if no stops are passed, returns the empty list" do
       assert excluded_origin_stops(1, "Route", []) == []
     end
+
+    test "excludes all terminals on westbound Green Line trips" do
+      assert excluded_origin_stops(0, "Green", []) == ["place-lake", "place-clmnl", "place-river", "place-hsmnl"]
+    end
+
+    test "excludes Lechmere on eastbound GL trips" do
+      all_stops = 1
+      |> GreenLine.stops_on_routes
+      |> GreenLine.all_stops
+
+      assert excluded_origin_stops(1, "Green", all_stops) == ["place-lech"]
+    end
   end
 
   describe "excluded_destination_stops/2" do
-    test "excludes nothing for non-Red lines" do
+    test "excludes nothing for non-Red or Green lines" do
       assert excluded_destination_stops("Green-B", "place-pktrm") == []
     end
 
@@ -35,6 +47,14 @@ defmodule ExcludedStopsTest do
 
     test "excludes Braintree stops if the origin is on the Ashmont branch" do
       assert "place-qamnl" in excluded_destination_stops("Red", "place-asmnl")
+    end
+
+    test "excludes stops on different branches of the consolidated Green Line" do
+      assert "place-prmnl" in excluded_destination_stops("Green", "place-lake")
+      assert "place-bland" in excluded_destination_stops("Green", "place-clmnl")
+      assert "place-smary" in excluded_destination_stops("Green", "place-river")
+      assert "place-fenwy" in excluded_destination_stops("Green", "place-hsmnl")
+      assert "place-spmnl" in excluded_destination_stops("Green", "place-hymnl")
     end
   end
 end
