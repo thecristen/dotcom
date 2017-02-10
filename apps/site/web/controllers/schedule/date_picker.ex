a6101e3209f03017d4434a7aeb93fe0dda6680cf
Paul Swartz <paulswartz@gmail.com>
2017-02-10 11:15:58 -0500

    use shift parameter in templates

--- apps/site/web/controllers/schedule/date_picker.ex
+++ apps/site/web/controllers/schedule/date_picker.ex
@@ -17,15 +17,26 @@ defmodule Site.ScheduleController.DatePicker do
     conn
   end
   def build_calendar(%Conn{assigns: %{date: date}} = conn, []) do
-    holidays = Holiday.Repo.holidays_in_month(date)
-    calendar = BuildCalendar.build(date, Util.service_date(), holidays, &update_url(conn, &1))
+    shift = shift(conn)
+    holidays = date |> Timex.shift(months: shift) |> Holiday.Repo.holidays_in_month()
+    calendar = BuildCalendar.build(date, Util.service_date(), holidays, &update_url(conn, &1), shift: shift)
 
     conn
-    |> assign(:holidays, holidays)
     |> assign(:calendar, calendar)
   end
 
   @spec show_datepicker?(Conn.t) :: boolean
   defp show_datepicker?(%Conn{query_params: %{"date_select" => "true"}}), do: true
   defp show_datepicker?(%Conn{}), do: false
+
+  defp shift(%Conn{query_params: %{"shift" => str}}) do
+    case Integer.parse(str) do
+      {integer, ""} ->
+        integer
+      _ -> 0
+    end
+  end
+  defp shift(%Conn{}) do
+    0
+  end
 end
