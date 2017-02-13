ad6555044ec91bfa18d659c3763705903baa691c
Paul Swartz <paulswartz@gmail.com>
2017-02-13 12:17:38 -0500

    pin pronto to 0.7.1 since 0.8.0 doesn't work with our runners yet

---
+++ gem.deps.rb
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'pronto', '~> 0.7.1'
+gem 'pronto-credo', '~> 0.0.4'
+gem 'pronto-eslint', '~> 0.7.0'
+gem 'pronto-scss', '~> 0.7.0'
+gem 'sass', '~> 3.4.23'
--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -15,7 +15,7 @@ mix local.rebar --force
 MIX_ENV=test mix do deps.get, deps.compile
 nvm use 6.2
 rbenv local 2.3
-GEM_SPEC=$SEMAPHORE_CACHE_DIR/gems gem install sass pronto pronto-credo pronto-eslint pronto-scss -N
+GEM_SPEC=$SEMAPHORE_CACHE_DIR/gems gem install -g gem.deps.rb sass pronto pronto-credo pronto-eslint pronto-scss -N
 NODEJS_ORG_MIRROR=$NVM_NODEJS_ORG_MIRROR npm run install
 npm run brunch:build
 MIX_ENV=test mix compile --force
