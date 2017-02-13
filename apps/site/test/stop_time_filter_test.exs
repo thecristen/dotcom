55866ee99d6d6a75592e978f5fc949f547eddcdf
Gene Shkolnik <gene@mbtace.com>
2017-02-13 11:14:44 -0500

    added more tests

---
+++ apps/site/test/stop_time_filter_test.exs
@@ -0,0 +1,73 @@
+defmodule StopTimeListFilterTest do
+  use ExUnit.Case, async: true
+  import StopTimeFilter
+
+  alias Schedules.Schedule
+
+  describe "StopTime.find_max_earlier_departure_schedule_time/2" do
+    test "finds max earlier departure time" do
+      stop_time1 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T07:00:00]}}}
+      stop_time2 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T08:00:00]}}}
+      stop_time3 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T09:00:00]}}}
+
+
+      # [ 7:00, 8:00, 9:00 ] @ 8:10 -> 8:00
+      assert find_max_earlier_departure_schedule_time([stop_time1, stop_time2, stop_time3], ~N[2017-03-01T08:10:00]) == ~N[2017-03-01T08:00:00]
+
+      # [ 7:00, 8:00, 9:00 ] @ 9:10 -> 9:00
+      assert find_max_earlier_departure_schedule_time([stop_time1, stop_time2, stop_time3], ~N[2017-03-01T09:10:00]) == ~N[2017-03-01T09:00:00]
+
+      # [ 7:00, 8:00, 9:00 ] @ 7:00 -> 7:00
+      assert find_max_earlier_departure_schedule_time([stop_time1, stop_time2, stop_time3], ~N[2017-03-01T07:00:00]) == ~N[2017-03-01T07:00:00]
+
+      # [ 7:00, 8:00, 9:00 ] @ 6:59 -> nil
+      assert find_max_earlier_departure_schedule_time([stop_time1, stop_time2, stop_time3], ~N[2017-03-01T06:59:00]) == nil
+
+    end
+  end
+
+  describe "StopTime.remove_departure_schedules_before/2" do
+    test "removes stop_times" do
+      stop_time1 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T07:00:00]}}}
+      stop_time2 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T08:00:00]}}}
+      stop_time3 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T09:00:00]}}}
+
+      # [ 7:00, 8:00, 9:00 ] before 8:10 -> [ 8:00, 9:00 ]
+      result1 = remove_departure_schedules_before([stop_time1, stop_time2, stop_time3], ~N[2017-03-01T07:10:00])
+      assert result1 == [ stop_time2, stop_time3 ]
+
+      # [ 9:00, 8:00, 7:00 ] before 8:10 -> [ 9:00, 8:00 ]
+      result2 = remove_departure_schedules_before([stop_time3, stop_time2, stop_time1], ~N[2017-03-01T07:10:00])
+      assert result2 == [ stop_time3, stop_time2 ]
+
+      # [ 9:00, 8:00, 7:00 ] before nil -> [ 7:00, 8:00, 9:00 ]
+      result3 = remove_departure_schedules_before([stop_time1, stop_time2, stop_time3], nil)
+      assert result3 == [ stop_time1, stop_time2, stop_time3 ]
+    end
+  end
+
+  describe "StopTime.filter/3" do
+    test "filters last trip and upcoming" do
+      stop_time1 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T07:00:00]}}}
+      stop_time2 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T08:00:00]}}}
+      stop_time3 = %StopTime{departure: %PredictedSchedule{schedule: %Schedule{time: ~N[2017-03-01T09:00:00]}}}
+
+      # [ 7:00, 8:00, 9:00 ] @ 7:10 -> [ 7:00, 8:00, 9:00 ]
+      result1 = filter([stop_time1, stop_time2, stop_time3], :last_trip_and_upcoming, ~N[2017-03-01T07:10:00])
+      assert result1 == [ stop_time1, stop_time2, stop_time3 ]
+
+      # [ 9:00, 8:00, 7:00 ] @ 8:10 -> [ 9:00, 8:00 ]
+      result2 = filter([stop_time3, stop_time2, stop_time1], :last_trip_and_upcoming, ~N[2017-03-01T08:10:00])
+      assert result2 == [ stop_time3, stop_time2 ]
+
+      # [ 9:00, 8:00, 7:00 ] @ 8:10 the day before -> [ 9:00, 8:00, 7:00 ]
+      result2 = filter([stop_time3, stop_time2, stop_time1], :last_trip_and_upcoming, ~N[2017-02-28T08:10:00])
+      assert result2 == [ stop_time3, stop_time2, stop_time1 ]
+
+      # [ 9:00, 8:00, 7:00 ] @ 8:10 the next day -> [ 9:00 ]
+      result3 = filter([stop_time3, stop_time2, stop_time1], :last_trip_and_upcoming, ~N[2017-03-02T08:10:00])
+      assert result3 == [ stop_time3 ]
+    end
+
+  end
+end
