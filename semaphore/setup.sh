459734094baa756a99692b3f814a4b20e844be39
Paul Swartz <paulswartz@gmail.com>
2017-02-13 13:29:04 -0500

    use ruby 2.4 since 2.3 seems to have other problems installing

--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -14,7 +14,7 @@ mix local.hex --force
 mix local.rebar --force
 MIX_ENV=test mix do deps.get, deps.compile
 nvm use 6.2
-rbenv local 2.3
+rbenv local 2.4
 GEM_SPEC=$SEMAPHORE_CACHE_DIR/gems gem install -g gem.deps.rb sass pronto pronto-credo pronto-eslint pronto-scss -N
 NODEJS_ORG_MIRROR=$NVM_NODEJS_ORG_MIRROR npm run install
 npm run brunch:build
