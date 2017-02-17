ff157c23bbd56550dc803ddf79c9ed3754ffca63
Paul Swartz <paulswartz@gmail.com>
2017-02-17 10:51:37 -0500

    remove now-unused image expand JS

--- apps/site/web/static/js/app.js
+++ apps/site/web/static/js/app.js
@@ -20,7 +20,6 @@
 // import socket from "./socket"
 import submitOnEvents from './submit-on-events.js';
 import dateToggle from './date-toggle.js';
-import imageExpand from './image-expand.js';
 import dismissAnnouncement from './dismiss-announcement.js';
 import selectModal from './select-modal.js';
 import tooltip from './tooltip.js';
@@ -40,7 +39,6 @@ import scrollTo from './scroll-to';
 
 submitOnEvents(["blur", "change"]);
 dateToggle();
-imageExpand();
 dismissAnnouncement();
 selectModal();
 tooltip();
