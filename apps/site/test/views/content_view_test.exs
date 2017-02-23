c3f8d26ccf4e74a2be82fef420280df3723abb15
Stephanie Viccari <stephviccari@gmail.com>
2017-02-23 13:35:17 -0500

    Updates the news entry template design
     - Adds a recent news section
     - Adds Mock, a mocking library for testing
     - Styles the image in the news entry body, should one be added by the
       CMS admin

--- apps/site/test/views/content_view_test.exs
+++ apps/site/test/views/content_view_test.exs
@@ -19,19 +19,19 @@ defmodule Site.ContentViewTest do
   describe "event_duration/2" do
     test "with no end time, only renders start time" do
       actual = event_duration(~N[2016-11-15T10:00:00], nil)
-      expected = "Tuesday, November 15th 10:00 AM"
+      expected = "November 15, 2016 10:00 AM"
       assert expected == actual
     end
 
     test "with start/end on same day, only renders date once" do
       actual = event_duration(~N[2016-11-14T12:00:00], ~N[2016-11-14T14:30:00])
-      expected = "Monday, November 14th 12:00 PM until 2:30 PM"
+      expected = "November 14, 2016 12:00 PM until 2:30 PM"
       assert expected == actual
     end
 
     test "with start/end on different days, renders both dates" do
       actual = event_duration(~N[2016-11-14T12:00:00], ~N[2016-12-01T14:30:00])
-      expected = "Monday, November 14th 12:00 PM until Thursday, December 1st 2:30 PM"
+      expected = "November 14, 2016 12:00 PM until December 1, 2016 2:30 PM"
       assert expected == actual
     end
 
@@ -40,7 +40,7 @@ defmodule Site.ContentViewTest do
         Timex.to_datetime(~N[2016-11-05T05:00:00], "Etc/UTC"),
         Timex.to_datetime(~N[2016-11-06T06:00:00], "Etc/UTC"))
       # could also be November 6th, 1:00 AM
-      expected = "Saturday, November 5th 1:00 AM until Sunday, November 6th 2:00 AM"
+      expected = "November 5, 2016 1:00 AM until November 6, 2016 2:00 AM"
       assert expected == actual
     end
   end
