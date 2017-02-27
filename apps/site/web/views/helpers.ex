17c68164f64adcac4b801db5e491f856c8e5d3b2
Ryan Mahoney <ryan@virtuecenter.com>
2017-02-27 15:47:51 -0500

    resolve alert wrap by using font-icon instead of SVG

--- apps/site/web/views/helpers.ex
+++ apps/site/web/views/helpers.ex
@@ -51,6 +51,7 @@ defmodule Site.ViewHelpers do
   @doc "HTML for a FontAwesome icon, with optional attributes"
   def fa(name, attributes \\ []) when is_list(attributes) do
     content_tag :i, [], [{:"aria-hidden", "true"},
+                         {:"style", Keyword.get(attributes, :style, "")},
                          {:class, "fa fa-#{name} " <> Keyword.get(attributes, :class, "")} |
                          Keyword.delete(attributes, :class)]
   end
