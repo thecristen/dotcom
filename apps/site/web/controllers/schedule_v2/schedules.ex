97e8661084b9706534218a3553de01cde71689ba
Paul Swartz <paulswartz@gmail.com>
2017-03-03 14:43:38 -0500

    reject non-pickup schedules

--- apps/site/web/controllers/schedule_v2/schedules.ex
+++ apps/site/web/controllers/schedule_v2/schedules.ex
@@ -37,7 +37,9 @@ defmodule Site.ScheduleV2Controller.Schedules do
                     direction_id: direction_id,
                     origin: %Stops.Stop{id: origin_id}}}) do
     # return schedules that stop at the origin
-    Schedules.Repo.all(stop: origin_id, route: route_id, date: date, direction_id: direction_id)
+    [stop: origin_id, route: route_id, date: date, direction_id: direction_id]
+    |> Schedules.Repo.all
+    |> Enum.reject(&match?(%Schedules.Schedule{pickup_type: 1}, &1))
   end
 
   @spec assign_frequency_table(Plug.Conn.t, [{Schedules.Schedule.t, Schedules.Schedule.t}]) :: Plug.Conn.t
