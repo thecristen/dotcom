cfdc92c10c8fac9aa34e7616a41136fd488c6dca
Stephanie Viccari <stephviccari@gmail.com>
2017-03-06 19:38:47 -0500

    Adds test coverage to ensure the correct html response is returned based on the CMS response
    - Adds the Mock library for testing
    - Verifies a 404 is returned when the CMS returns an error

--- apps/site/mix.exs
+++ apps/site/mix.exs
@@ -85,7 +85,8 @@ defmodule Site.Mixfile do
      {:vehicles, in_umbrella: true},
      {:google_maps, in_umbrella: true},
      {:floki, "~> 0.12.0", only: :test},
-     {:mochiweb, "~> 2.15.0", override: true}]
+     {:mochiweb, "~> 2.15.0", override: true},
+     {:mock, "~> 0.2.0", only: :test}]
       # NOTE: mochiweb override added to resolve dependency conflict
       # between html_sanitize_ex (2.12.2) and floki (2.15.0). Overriding does not
       # affect the functions we currently use html_sanitize_ex for. This should be
