e0ca9a1d6b0b2e1e7106ee672362d298998d90bf
Paul Swartz <paulswartz@gmail.com>
2017-03-09 15:50:45 -0500

    disable more servers in CI

--- semaphore/setup.sh
+++ semaphore/setup.sh
@@ -5,8 +5,10 @@ ERLANG_VERSION=19
 mkdir -p $SEMAPHORE_CACHE_DIR/gems $SEMAPHORE_CACHE_DIR/npm $SEMAPHORE_CACHE_DIR/mix
 
 # Turn off some high-memory apps
-test -x /etc/init.d/cassandra && sudo /etc/init.d/cassandra stop
-test -x /etc/init.d/elasticsearch && sudo /etc/init.d/elasticsearch stop
+for service in cassandra elasticsearch mysql rabbitmq-server mongod docker memcached postgresql apache2 redis-server; do
+    sudo service $service stop
+done
+killall Xvfb
 
 export MIX_HOME=$SEMAPHORE_CACHE_DIR/mix
 
