93c58a8481706a18090b0bcfb137aaea106607d9
Gabe Durazo <gabe@durazo.us>
2017-03-14 15:31:51 -0500

    Address some PR comments
    * Change links to https
    * Add schedules for subways
    * Put related tests in describe block

--- apps/site/test/controllers/schedule_v2/pdf_test.exs
+++ apps/site/test/controllers/schedule_v2/pdf_test.exs
@@ -3,7 +3,7 @@ defmodule Site.ScheduleV2Controller.PdfTest do
 
   test "redirects to PDF for route when present", %{conn: conn} do
     conn = get(conn, route_pdf_path(conn, :pdf, %Routes.Route{id: "CR-Fitchburg"}))
-    assert redirected_to(conn, 302) == "http://www.mbta.com/uploadedfiles/Documents/Schedules_and_Maps/Commuter_Rail/fitchburg.pdf"
+    assert redirected_to(conn, 302) == "https://www.mbta.com/uploadedfiles/Documents/Schedules_and_Maps/Commuter_Rail/fitchburg.pdf"
   end
 
   test "renders 404 if route doesn't exist", %{conn: conn} do
@@ -12,7 +12,7 @@ defmodule Site.ScheduleV2Controller.PdfTest do
   end
 
   test "renders 404 if route exists but does not have PDF", %{conn: conn} do
-    conn = get(conn, route_pdf_path(conn, :pdf, %Routes.Route{id: "Red"}))
+    conn = get(conn, route_pdf_path(conn, :pdf, %Routes.Route{id: "57A"}))
     assert html_response(conn, 404)
   end
 end
