8585f92729db996bfbbccf370f092f76bdd6eade
Paul Swartz <paulswartz@gmail.com>
2017-03-14 07:34:08 -0400

    link to the alert's URL (if present) in the banner

--- apps/alerts/lib/banner.ex
+++ apps/alerts/lib/banner.ex
@@ -1,11 +1,13 @@
 defmodule Alerts.Banner do
   defstruct [
     id: "",
-    title: ""
+    title: "",
+    url: nil
   ]
 
   @type t :: %__MODULE__{
     id: String.t,
-    title: String.t
+    title: String.t,
+    url: String.t | nil
   }
 end
