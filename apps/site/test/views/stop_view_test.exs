3fd2a83e448a7068b296c1bb9a81637d8f846132
kohlerjp <john@mbtace.com>
2017-04-07 13:12:10 -0400

    stopview cleanup

--- apps/site/test/views/stop_view_test.exs
+++ apps/site/test/views/stop_view_test.exs
@@ -1,54 +1,40 @@
 defmodule Site.StopViewTest do
   @moduledoc false
-  alias Site.StopView
+  import Site.StopView
   alias Stops.Stop
   use Site.ConnCase, async: true
 
   describe "fare_group/1" do
     test "return correct fare group for all modes" do
-      assert StopView.fare_group(:bus) == "bus_subway"
-      assert StopView.fare_group(:subway) == "bus_subway"
-      assert StopView.fare_group(:commuter_rail) == "commuter_rail"
-      assert StopView.fare_group(:ferry) == "ferry"
+      assert fare_group(:bus) == "bus_subway"
+      assert fare_group(:subway) == "bus_subway"
+      assert fare_group(:commuter_rail) == "commuter_rail"
+      assert fare_group(:ferry) == "ferry"
     end
   end
 
   describe "template_for_tab/1" do
     test "correct template for selected tab" do
-      assert StopView.template_for_tab(nil) == "_schedule.html"
-      assert StopView.template_for_tab("info") == "_info.html"
-      assert StopView.template_for_tab("schedule") == "_schedule.html"
+      assert template_for_tab(nil) == "_schedule.html"
+      assert template_for_tab("info") == "_info.html"
+      assert template_for_tab("schedule") == "_schedule.html"
     end
   end
 
   describe "tab_selected?/2" do
     test "true for the selected tab, false otherwise" do
-      assert StopView.tab_selected?("schedule", nil)
-      refute StopView.tab_selected?("info", nil)
-      refute StopView.tab_selected?("schedule", "info")
-      assert StopView.tab_selected?("info", "info")
-    end
-  end
-
-  describe "show_fares?/1" do
-    test "Do not show fare information for Origin stations" do
-      north_station = %Stop{id: "place-north"}
-      back_bay = %Stop{id: "place-bbsta"}
-      salem = %Stop{id: "Salem"}
-      westborough = %Stop{id: "Westborough"}
-
-      assert !StopView.show_fares?(north_station)
-      assert !StopView.show_fares?(back_bay)
-      assert StopView.show_fares?(salem)
-      assert StopView.show_fares?(westborough)
+      assert tab_selected?("schedule", nil)
+      refute tab_selected?("info", nil)
+      refute tab_selected?("schedule", "info")
+      assert tab_selected?("info", "info")
     end
   end
 
   describe "fare_mode/1" do
     test "types are separated as bus, subway or both" do
-      assert StopView.fare_mode([:bus, :commuter_rail, :ferry]) == :bus
-      assert StopView.fare_mode([:subway, :commuter_rail, :ferry]) == :subway
-      assert StopView.fare_mode([:subway, :commuter_rail, :ferry, :bus]) == :bus_subway
+      assert fare_mode([:bus, :commuter_rail, :ferry]) == :bus
+      assert fare_mode([:subway, :commuter_rail, :ferry]) == :subway
+      assert fare_mode([:subway, :commuter_rail, :ferry, :bus]) == :bus_subway
     end
   end
 
@@ -60,10 +46,32 @@ defmodule Site.StopViewTest do
       many_feature = %Stop{name: "test", accessibility: ["accessible", "ramp", "elevator"]}
       tag_has_text = fn(tag, text) -> Phoenix.HTML.safe_to_string(Enum.at(tag, 0)) =~ text end
 
-      assert tag_has_text.(StopView.accessibility_info(no_accessibility), "No accessibility")
-      assert tag_has_text.(StopView.accessibility_info(no_accessible_feature), "No accessibility")
-      assert tag_has_text.(StopView.accessibility_info(only_accessible_feature), "is an accessible station")
-      assert tag_has_text.(StopView.accessibility_info(many_feature), "has the following")
+      assert tag_has_text.(accessibility_info(no_accessibility), "No accessibility")
+      assert tag_has_text.(accessibility_info(no_accessible_feature), "No accessibility")
+      assert tag_has_text.(accessibility_info(only_accessible_feature), "is an accessible station")
+      assert tag_has_text.(accessibility_info(many_feature), "has the following")
+    end
+  end
+
+  describe "pretty_accessibility/1" do
+    test "formats phone and escalator fields" do
+      assert pretty_accessibility("tty_phone") == "TTY Phone"
+      assert pretty_accessibility("escalator_both") == "Escalator (Both)"
+    end
+
+    test "For all other fields, separates underscore and capitalizes all words" do
+      assert pretty_accessibility("elevator_issues") == "Elevator Issues"
+      assert pretty_accessibility("down_escalator_repair_work") == "Down Escalator Repair Work"
+    end
+  end
+
+  describe "sort_parking_spots/1" do
+    test "parkings spots are sorted in correct order" do
+      basic_spot = %{type: "basic"}
+      accessible_spot = %{type: "accessible"}
+      free_spot = %{type: "free"}
+      sorted = sort_parking_spots([free_spot, basic_spot, accessible_spot])
+      assert sorted == [basic_spot, accessible_spot, free_spot]
     end
   end
 
@@ -74,47 +82,45 @@ defmodule Site.StopViewTest do
       c_line = %{name: "Green-C"}
       orange_line = %{name: "Orange"}
       line_list = [e_line, d_line, c_line, orange_line]
-      green_count = line_list |> Site.StopView.aggregate_routes |> Enum.filter(&(&1.name == "Green")) |> Enum.count
+      green_count = line_list |> aggregate_routes |> Enum.filter(&(&1.name == "Green")) |> Enum.count
 
       assert green_count == 1
-      assert Enum.count(Site.StopView.aggregate_routes(line_list)) == 2
+      assert Enum.count(aggregate_routes(line_list)) == 2
     end
   end
 
   describe "schedule_display_time/2" do
+    @time ~N[2017-01-01T22:30:00]
     test "returns difference in minutes when difference is less than 60" do
-      now = Util.now
-      diff = now
+      diff = @time
       |> Timex.shift(minutes: 10)
-      |> StopView.schedule_display_time(now)
+      |> schedule_display_time(@time)
       assert diff == "10 mins"
     end
 
     test "returns formatted time when difference is greater than 60" do
-      now = Util.now
-      time = Timex.shift(now, hours: 2)
-      diff = time
-      |> StopView.schedule_display_time(now)
-      assert diff == time |> formatted_time
+      two_hours_later = Timex.shift(@time, hours: 2)
+      diff = schedule_display_time(two_hours_later, @time)
+      assert diff == "12:30AM"
     end
   end
 
   describe "center_query/1" do
     test "returns a marker at the stop if it only has buses" do
       stop = %Stop{id: "2438", latitude: "42.37497", longitude: "-71.102529"}
-      assert Site.StopView.center_query(stop) == [markers: "42.37497,-71.102529"]
+      assert center_query(stop) == [markers: "42.37497,-71.102529"]
     end
 
     test "returns the location of the stop as the map center if it serves other modes" do
       stop = %Stop{id: "place-sstat", latitude: "42.352271", longitude: "-71.055242"}
-      assert Site.StopView.center_query(stop) == [markers: Site.StopView.location(stop)]
+      assert center_query(stop) == [markers: location(stop)]
     end
   end
 
   describe "fare_surcharge?/1" do
     test "returns true for South, North, and Back Bay stations" do
       for stop_id <- ["place-bbsta", "place-north", "place-sstat"] do
-        assert Site.StopView.fare_surcharge?(%Stop{id: stop_id})
+        assert fare_surcharge?(%Stop{id: stop_id})
       end
     end
   end
@@ -124,7 +130,7 @@ defmodule Site.StopViewTest do
       grouped_routes = [bus: [%Routes.Route{direction_names: %{0 => "Outbound", 1 => "Inbound"},
         id: "742", key_route?: true, name: "SL2", type: 3}]]
 
-      assert Site.StopView.info_tab_name(grouped_routes) == "Stop Info"
+      assert info_tab_name(grouped_routes) == "Stop Info"
     end
 
     test "is station info when given any other line" do
@@ -135,11 +141,9 @@ defmodule Site.StopViewTest do
           id: "Red", key_route?: true, name: "Red", type: 1}]
       ]
 
-      assert Site.StopView.info_tab_name(grouped_routes) == "Station Info"
+      assert info_tab_name(grouped_routes) == "Station Info"
     end
   end
-
-  def formatted_time(time), do: time |> Timex.format!("{h12}:{m} {AM}")
 
   def do_safe_to_string(elements) when is_list(elements) do
     elements
