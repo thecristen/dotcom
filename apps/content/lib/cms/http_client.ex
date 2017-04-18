c3e135b5528c058d67b9b94ffcd1752faac01ef1
Paul Swartz <paulswartz@gmail.com>
2017-04-18 13:32:14 -0400

    log timing info from CMS

--- apps/content/lib/cms/http_client.ex
+++ apps/content/lib/cms/http_client.ex
@@ -1,10 +1,13 @@
 defmodule Content.CMS.HTTPClient do
   @behaviour Content.CMS
+  require Logger
 
   def view(path, params \\ []) do
     params = Keyword.merge(params, [_format: "json"])
     with {:ok, url} <- make_url(path),
-      {:ok, %{status_code: 200, body: body}} <- HTTPoison.get(url, [], params: params),
+      {time, response} <- :timer.tc(HTTPoison, :get, [url, [], [params: params]]),
+      log_response(time, url, params, response),
+      {:ok, %{status_code: 200, body: body}} <- response,
       {:ok, parsed} <- Poison.Parser.parse(body) do
       {:ok, parsed}
     else
@@ -22,5 +25,17 @@ defmodule Content.CMS.HTTPClient do
     else
       {:error, :no_root}
     end
+  end
+
+  defp log_response(time, url, params, response) do
+    _ = Logger.info(fn ->
+      params = Keyword.delete(params, :_format)
+      text = case response do
+               {:ok, %{status_code: code}} -> "status=#{code}"
+               {:error, e} -> "status=error error=#{inspect e}"
+             end
+      time = time / :timer.seconds(1)
+      "Content.CMS.HTTPClient_response url=#{url} params=#{inspect params} #{text} duration=#{time}"
+    end)
   end
 end
