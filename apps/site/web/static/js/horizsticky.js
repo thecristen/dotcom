4cd6b4cdc60c73691fc4b25bb54704325a2f3dca
Paul Swartz <paulswartz@gmail.com>
2017-04-19 12:37:39 -0400

    do a better job handling resizes

--- apps/site/web/static/js/horizsticky.js
+++ apps/site/web/static/js/horizsticky.js
@@ -12,18 +12,20 @@ export default function($) {
     // reset scroll position
     $this.scrollLeft(0);
 
-    $this.find("[data-sticky]").each(function() {
-      // reset the width/height of the element
-      const $child = $(this);
-      const sticky = $child.data("sticky");
-      const rect = {width: $child.outerWidth(), height: $child.outerHeight()};
+    $this.find("[data-sticky]")
+      .css({width: 'auto', height: 'auto', position: 'relative'}) // reset CSS
+      .each((_index, el) => {
+        console.log(el);
+        const $child = $(el);
+        const sticky = $child.data("sticky");
+        const rect = el.getBoundingClientRect();
 
-      if (sticky === "left") {
-        queue.push(() => leftSticky($, $child, rect));
-      } else if (sticky === "right") {
-        queue.push(() => rightSticky($, $child, rect));
-      }
-    });
+        if (sticky === "left") {
+          queue.push(() => leftSticky($, $child, rect));
+        } else if (sticky === "right") {
+          queue.push(() => rightSticky($, $child, rect));
+        }
+      });
 
     // batch all the CSS updates
     $.each(queue, (_index, fn) => fn());
@@ -37,24 +39,23 @@ export default function($) {
   $(window).on('resize', bindScrollContainers);
 }
 
-function maybeReplace($child) {
-  // clear a previous sticky replacement
-  var $replacement = $child.prev();
-  if ($replacement.hasClass("sticky-replacement")) {
-    $replacement.remove();
+function makeReplacement($, $child) {
+  // we add a replacement element so we can push sibling elements out of
+  // the way once we've gone absolute
+  var $replacement = $child.prev(".sticky-replacement");
+  if ($replacement.length === 0) {
+    $replacement = $("<" + $child[0].tagName + ">").addClass("sticky-replacement").text(' ');
+    $replacement.insertBefore($child);
   }
+  return $replacement;
 }
 
 function leftSticky($, $child, rect) {
-  maybeReplace($child);
-  // we add a replacement element so we can push sibling elements out of
-  // the way once we've gone absolute
-  $("<" + $child[0].tagName + ">").addClass("sticky-replacement").text(' ')
+  makeReplacement($, $child)
     .css({
       paddingLeft: Math.ceil(rect.width),
       height: Math.ceil(rect.height)
-    })
-    .insertBefore($child);
+    });
   // update our CSS position/size
   $child.css({
     height: Math.ceil(rect.height),
@@ -65,13 +66,11 @@ function leftSticky($, $child, rect) {
 }
 
 function rightSticky($, $child, rect) {
-  maybeReplace($child);
-  $("<" + $child[0].tagName + ">").addClass("sticky-replacement").text(' ')
+  makeReplacement($, $child)
     .css({
       paddingRight: Math.ceil(rect.width),
       height: Math.ceil(rect.height)
-    })
-    .insertBefore($child);
+    });
 
   $child.css({
     height: Math.ceil(rect.height),
