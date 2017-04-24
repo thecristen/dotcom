b7e92dcbecd1c92f102a9ce555c224281d7cd313
Paul Swartz <paulswartz@gmail.com>
2017-04-24 08:10:14 -0400

    give PDF URLs a start date

--- apps/routes/lib/pdf.ex
+++ apps/routes/lib/pdf.ex
@@ -1,13 +1,55 @@
 defmodule Routes.Pdf do
   alias Routes.Route
+  alias Routes.Pdf.Helpers
 
-  @routes_to_pdfs "priv/pdfs.csv"
+  @filename "priv/pdfs.csv"
+  @external_resource @filename
+
+  @routes_to_pdfs @filename
     |> File.stream!
     |> CSV.decode
 
+  @doc "Returns a URL for a PDF of the given schedule."
   @spec url(Route.t) :: String.t | nil
-  for [route_id, pdf_url] <- @routes_to_pdfs do
-    def url(%Route{id: unquote(route_id)}), do: unquote(pdf_url)
+  def url(%Route{id: route_id}) do
+    case do_dated_urls(route_id) do
+      [{_, url} | _] -> url
+      _ -> nil
+    end
   end
-  def url(%Route{}), do: nil
+
+  @doc """
+
+  Returns a list of PDF URLs, along with the date that the schedule starts
+  being valid.  The provided date is used to filter out PDFs which are no
+  longer valid.
+
+  """
+  @spec dated_urls(Route.t, Date.t) :: [{Date.t, String.t}]
+  def dated_urls(%Route{id: route_id}, date) do
+    route_id
+    |> do_dated_urls
+    |> filter_outdated(date)
+  end
+
+  for {route_id, rows} <- Enum.group_by(@routes_to_pdfs, &hd/1) do
+    parsed = Enum.map(rows, &Helpers.parse_date/1)
+    defp do_dated_urls(unquote(route_id)), do: unquote(Macro.escape(parsed))
+  end
+  defp do_dated_urls(_route_id), do: []
+
+  defp filter_outdated([], _) do
+    []
+  end
+  defp filter_outdated([_] = single, _) do
+    single
+  end
+  defp filter_outdated([previous, next | rest], date) do
+    {next_date, _} = next
+    if Date.compare(next_date, date) == :gt do
+      [previous] ++ filter_outdated([next | rest], date)
+    else
+      [next | rest]
+    end
+  end
 end
