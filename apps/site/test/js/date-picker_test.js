ce981a5f296dc0c468703d0ef6f99dd3c045b332
Ryan Mahoney <ryan@virtuecenter.com>
2017-04-25 12:05:19 -0400

    frontend test coverage

---
+++ apps/site/test/js/date-picker_test.js
@@ -0,0 +1,31 @@
+import { assert } from 'chai';
+import jsdom from 'mocha-jsdom';
+import datePicker from '../../web/static/js/date-picker';
+
+describe('date-picker', () => {
+  var $;
+
+  jsdom();
+
+  beforeEach(() => {
+    $ = jsdom.rerequire('jquery');
+    $('body').html(
+      '<div class="date-picker-toggle"></div><div class="date-picker-container">Date Picker</div><div class="calendar-cover"></div>'
+    );
+    datePicker($);
+  });
+
+  it('hides date picker by default', () => {
+
+    console.log(window.location.search);
+
+    assert.isTrue($('.date-picker-container')[0].hasAttribute("hidden"));
+    assert.isTrue($('.calendar-cover')[0].hasAttribute("hidden"));
+  });
+
+  it('displays date picker with clicked', () => {
+    $('.date-picker-toggle').click();
+    assert.isFalse($('.date-picker-container')[0].hasAttribute("hidden"));
+    assert.isFalse($('.calendar-cover')[0].hasAttribute("hidden"));
+  });
+});
