ba27f385369bfbda3f86bb6129221ffed91827ff
Gabe Durazo <gabe@durazo.us>
2017-05-01 14:04:24 -0400

    Update news app script and README

--- apps/news/upload_to_s3.sh
+++ apps/news/upload_to_s3.sh
@@ -7,8 +7,12 @@
 # * MBTA_SQL_PASSWORD
 # * MBTA_SQL_DATABASE
 # * S3_BUCKET
+# Run from dotcom/apps/news/ directory
 
 # number of posts to include
 POST_COUNT=10
 
-../../aws/bin/aws s3 cp posts.zip s3://$S3_BUCKET/posts.zip --acl=public-read
+./import_posts.py priv/posts
+test -f posts.zip && rm -f posts.zip
+find priv/posts | sort | tail -n $POST_COUNT | xargs zip -9j posts.zip
+aws s3 cp posts.zip s3://$S3_BUCKET/posts.zip --acl=public-read
