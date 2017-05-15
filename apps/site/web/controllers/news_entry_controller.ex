3f8b6de671dd1ee31230257497121488c5ae5645
Stephanie Viccari <stephviccari@gmail.com>
2017-05-15 13:46:35 -0400

    Excludes the current News Entry from the Recent News suggestions
    Why:
    When viewing a news entry, we include a section the recommends other
    recent news. The recent news suggestions should not include the current
    news entry.
    
    This PR:
    Removes the current news entry from the list of recent news suggestions.

--- apps/site/web/controllers/news_entry_controller.ex
+++ apps/site/web/controllers/news_entry_controller.ex
@@ -3,7 +3,7 @@ defmodule Site.NewsEntryController do
 
   def show(conn, %{"id" => id}) do
     news_entry = Content.Repo.news_entry!(id)
-    recent_news = Content.Repo.recent_news()
+    recent_news = Content.Repo.recent_news(current_id: news_entry.id)
 
     conn
     |> assign(:news_entry, news_entry)
