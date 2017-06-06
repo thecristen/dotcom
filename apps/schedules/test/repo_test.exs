83c903456eeb0636563e4d2f0d449f2203962672
Ryan Mahoney <ryan@virtuecenter.com>
2017-06-06 15:54:13 -0400

    update test per code review

--- apps/schedules/test/repo_test.exs
+++ apps/schedules/test/repo_test.exs
@@ -92,13 +92,13 @@ defmodule Schedules.RepoTest do
       assert trip("invalid ID") == nil
     end
 
-    test "returns a %Schedule.Trip{} for a given ID with a shape_id" do
+    test "returns a %Schedule.Trip{} for a given ID" do
       date = Timex.shift(Util.service_date, days: 1)
       schedules = by_route_ids(["1"], date: date, stop_sequences: :first, direction_id: 0)
       scheduled_trip = List.first(schedules).trip
       trip = trip(scheduled_trip.id)
       assert scheduled_trip == trip
-      assert trip.shape_id != nil
+      refute trip.shape_id == nil
     end
   end
 
