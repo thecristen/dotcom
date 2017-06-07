e796b38eaed34ddbce1303b573dbf288048a1909
Alex Sghia-Hughes <asghiah@gmail.com>
2017-06-07 16:23:31 -0400

    improve display for schedule tab on station page (#1235)
    * make the footer stickier and give a good message when we dont have anything to show on the stops schedules page
    
    * fix front page footer display bug
    
    * linter
    
    * add padding on the message when there are no schedules to display
    
    * add test for beta announcement link
    
    * backstop update
    
    * pipe chain
    
    * add a space around the operator

--- apps/site/test/views/layout_view_test.exs
+++ apps/site/test/views/layout_view_test.exs
@@ -5,14 +5,6 @@ defmodule Site.LayoutViewTest do
 
   import Site.LayoutView
 
-  describe "_beta_announcement.html" do
-    test "links to the feedback form", %{conn: conn} do
-      conn = fetch_query_params(conn)
-      output = "_beta_announcement.html" |> render(conn: conn) |> safe_to_string
-      assert output =~ Site.ViewHelpers.feedback_form_url()
-    end
-  end
-
   describe "bold_if_active/3" do
     test "bold_if_active makes text bold if the current request is made against the given path", %{conn: conn} do
       conn = %{conn | request_path: "/schedules/subway"}
@@ -32,5 +24,15 @@ defmodule Site.LayoutViewTest do
 
     expected_title = "Subway < Schedules & Maps < MBTA - Massachusetts Bay Transportation Authority"
     assert body =~ "<title>#{expected_title}</title>"
+  end
+
+  test "beta announcement links the feedback form", %{conn: conn} do
+    conn = get conn, "/"
+    body = conn
+           |> html_response(200)
+           |> Floki.find(".beta-announcement-link")
+           |> Floki.attribute("href")
+
+    assert body == [Site.ViewHelpers.feedback_form_url()]
   end
 end
