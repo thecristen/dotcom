3990147c313ded698359884223be304e5c0070fc
Ryan Mahoney <ryan@virtuecenter.com>
2017-06-19 13:08:13 -0400

    animate scroll of timetable

--- apps/site/web/static/js/timetable-scroll.js
+++ apps/site/web/static/js/timetable-scroll.js
@@ -1,14 +1,32 @@
 export default function($) {
   $ = $ || window.jQuery;
 
-  function setupTimetableScroll() {
-    $(document).on(
-      'click', '.schedule-v2-timetable-more-col-earlier',
-      function(ev) { $(ev.target).closest('[data-sticky-container]')[0].scrollLeft -= 100;});
-    $(document).on(
-      'click', '.schedule-v2-timetable-more-col-later',
-      function(ev) { $(ev.target).closest('[data-sticky-container]')[0].scrollLeft += 100;});
+  const scroll = (ev) => {
+    // determine scroll direction based on element clicked
+    var multiplier;
+    var $boundEl = $(ev.target);
+    while (true) {
+      if (!$boundEl || $boundEl.hasClass("schedule-v2-timetable-more-col")) {
+        multiplier = ($boundEl.hasClass("schedule-v2-timetable-more-col-earlier")) ? -1 : 1;
+        break;
+      }
+      $boundEl = $boundEl.parent();
+    }
+
+    // dynamically determine width of column based on width of an existing column
+    const offset = $(".schedule-v2-timetable-header-col.schedule-v2-timetable-time-col").eq(0).outerWidth() * multiplier;
+
+    // find the container element to be scrolled
+    const $el = $(ev.target).closest("[data-sticky-container]");
+
+    // animate the scroll element
+    $el.animate({scrollLeft: $el.scrollLeft() + offset});
+  };
+
+  function addClickHandlers() {
+    $(document).on("click", ".schedule-v2-timetable-more-col-earlier", scroll);
+    $(document).on("click", ".schedule-v2-timetable-more-col-later", scroll);
   }
 
-  setupTimetableScroll();
+  addClickHandlers();
 };
