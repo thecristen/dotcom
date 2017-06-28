e7349a5212c547903a78e20e1c03e72e7906f756
Paul Swartz <paulswartz@gmail.com>
2017-06-28 14:26:26 -0400

    more error rendering

--- apps/site/web/views/trip_plan_view.ex
+++ apps/site/web/views/trip_plan_view.ex
@@ -1,11 +1,33 @@
 defmodule Site.TripPlanView do
   use Site.Web, :view
-  alias TripPlan.{Leg, TransitDetail, PersonalDetail}
+  alias TripPlan.{Leg, TransitDetail, PersonalDetail, Query}
 
-  @spec rendered_location_error(Plug.Conn.t, TripPlan.Query.t | nil, :from | :to) :: Phoenix.HTML.safe
+  @spec rendered_location_error(Plug.Conn.t, Query.t | nil, :from | :to) :: Phoenix.HTML.Safe.t
   def rendered_location_error(conn, query_or_nil, location_field)
-  def rendered_location_error(_conn, _query_or_nil, _location_field) do
+  def rendered_location_error(_conn, nil, _location_field) do
     ""
+  end
+  def rendered_location_error(%Plug.Conn{} = conn, %Query{} = query, field) when field in [:from, :to] do
+    case Map.get(query, field) do
+      {:error, error} ->
+        do_render_location_error(conn, field, error)
+      _ ->
+        ""
+    end
+  end
+
+  @spec do_render_location_error(Plug.Conn.t, :from | :to, TripPlan.Geocode.error) :: Phoenix.HTML.Safe.t
+  defp do_render_location_error(_conn, _field, :no_results) do
+    "That address was not found. Please try a different address."
+  end
+  defp do_render_location_error(conn, field, {:too_many_results, results}) do
+    render "_error_too_many_results.html", conn: conn, field: field, results: results
+  end
+  defp do_render_location_error(_conn, _field, :required) do
+    "This field is required."
+  end
+  defp do_render_location_error(_conn, _field, :unknown) do
+    "An unknown error occurred. Please try again, or try a different address."
   end
 
   def optional_position({:ok, _}), do: ""
