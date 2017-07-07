5f39daa454aa0911f4122586c71fbe0aee368612
Stephanie Viccari <stephviccari@gmail.com>
2017-07-07 16:44:10 -0400

    Trigger tests to fail if the test emits a warning
    This PR:
    Updates the test helper for each app so devs are alerted, in a highly
    visible manner, that test code generates an warning.

--- apps/alerts/test/test_helper.exs
+++ apps/alerts/test/test_helper.exs
@@ -2,3 +2,6 @@
 {:ok, _} = Application.ensure_all_started(:tzdata)
 _ = Tzdata.ReleaseUpdater.poll_for_update()
 ExUnit.start()
+
+# Report warnings as errors
+Code.compiler_options(warnings_as_errors: true)
