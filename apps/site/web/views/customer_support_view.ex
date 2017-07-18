6de849e471616e60141848624daa390e8cd0e008
Ryan Mahoney <ryan@virtuecenter.com>
2017-07-18 09:35:13 -0400

    remove conditions from template

--- apps/site/web/views/customer_support_view.ex
+++ apps/site/web/views/customer_support_view.ex
@@ -15,4 +15,9 @@ defmodule Site.CustomerSupportView do
   def show_error_message(conn) do
     conn.assigns.show_form && !Enum.empty?(conn.assigns[:errors])
   end
+
+  @spec class_for_error(String.t, [], String.t, String.t) :: String.t
+  def class_for_error(value, errors, on_class, off_class) do
+    if Enum.member?(errors, value), do: on_class, else: off_class
+  end
 end
