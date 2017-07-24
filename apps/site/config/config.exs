d9c9cc72fb2feeaf029202a97ae0fbd0a796be7d
Kate Hedgpeth <kate@mbtace.com>
2017-07-24 10:15:15 -0400

    remove flag for google translate plugin

--- apps/site/config/config.exs
+++ apps/site/config/config.exs
@@ -25,9 +25,7 @@ config :site, Site.ViewHelpers,
   feedback_form_url: "https://docs.google.com/a/mbtace.com/forms/d/e/1FAIpQLScjM7vVFw-5qNZsKC3CNy7xzOAg0i5atkn_tWhkzZkw_oQUyg/viewform"
 
 config :laboratory,
-  features: [
-    {:google_translate, "Google Translate", "Adds the Google Translate plugin"}
-  ],
+  features: [],
   cookie: [
     max_age: 3600 * 24 * 30, # one month,
     http_only: true
