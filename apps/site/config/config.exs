179293660e7e4bcda7caaca95835e0ee31387ca8
Kate Hedgpeth <katehedgpeth@gmail.com>
2017-07-26 10:24:54 -0400

    don't expose google translate yet (#1478)

--- apps/site/config/config.exs
+++ apps/site/config/config.exs
@@ -25,7 +25,7 @@ config :site, Site.ViewHelpers,
   feedback_form_url: "https://docs.google.com/a/mbtace.com/forms/d/e/1FAIpQLScjM7vVFw-5qNZsKC3CNy7xzOAg0i5atkn_tWhkzZkw_oQUyg/viewform"
 
 config :laboratory,
-  features: [],
+  features: [{:google_translate, "Google Translate", "Adds the Google Translate plugin"}],
   cookie: [
     max_age: 3600 * 24 * 30, # one month,
     http_only: true
