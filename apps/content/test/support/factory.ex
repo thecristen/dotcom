e9103bbd222bded65e1d82737405e279b6303c87
Stephanie Viccari <stephviccari@gmail.com>
2017-08-14 17:39:50 -0400

    Update the content factory to use a more appropriate fixtured response
    Why:
    When the person factory was introduced, the CMS API only returned the
    person record within the people grid paragraph. This is no longer true
    as the API now includes the endpoint api/people.
    
    This PR:
    Updates the content person factory to use the stubbed json response to
    api/people. This revised approach is more straight forward and saves us
    unnecessary work of parsing a person record from the stubbed paragraphs
    response.

--- apps/content/test/support/factory.ex
+++ apps/content/test/support/factory.ex
@@ -12,18 +12,9 @@ defmodule Content.Factory do
   end
 
   def person_factory(options \\ %{}) do
-    person_json = person_from_people_grid_paragraph()
-
-    person_json
+    Content.CMS.Static.people_response()
+    |> List.first
     |> Content.Person.from_api()
     |> Map.merge(options)
-  end
-
-  defp person_from_people_grid_paragraph do
-    Content.CMS.Static.all_paragraphs_response()
-    |> Map.get("field_paragraphs")
-    |> Enum.find(& match?(%{"type" => [%{"target_id" => "people_grid"}]}, &1))
-    |> Map.get("field_people")
-    |> List.first()
   end
 end
