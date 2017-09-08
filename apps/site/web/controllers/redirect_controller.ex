bd9c8ed4b528f5d2d551a5739d644da6267d5ea7
Gene Shkolnik <gene@mbtace.com>
2017-09-08 14:50:50 -0400

    redirect business center to bc.mbta.com/business_center

--- apps/site/web/controllers/redirect_controller.ex
+++ apps/site/web/controllers/redirect_controller.ex
@@ -20,6 +20,7 @@ defmodule Site.RedirectController do
   defp get_path([], _params), do: "http://old.mbta.com/"
   defp get_path(["pass_program" | parts], params), do: "https://passprogram.mbta.com/#{parts_to_path(parts, params)}"
   defp get_path(["commerce" | parts], params), do: "https://commerce.mbta.com/#{parts_to_path(parts, params)}"
+  defp get_path(["business_center" | parts], params), do: "http://bc.mbta.com/business_center/#{parts_to_path(parts, params)}"
   defp get_path(parts, params), do: "http://old.mbta.com/#{parts_to_path(parts, params)}"
 
   @spec parts_to_path([String.t], map()) :: String.t
