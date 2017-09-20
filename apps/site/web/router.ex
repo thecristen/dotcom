eb5e0f420d12a07d978e6fe2d6f7183131defb8c
Ryan Mahoney <ryan@virtuecenter.com>
2017-09-20 14:42:48 -0400

    remove comment

--- apps/site/web/router.ex
+++ apps/site/web/router.ex
@@ -26,11 +26,6 @@ defmodule Site.Router do
     plug Site.Plugs.RewriteUrls
   end
 
-  # Use this pipeline when you need to defend against CSRF attacks
-  # pipeline :csrf do
-  #   plug :protect_from_forgery
-  # end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
