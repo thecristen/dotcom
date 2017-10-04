744967c81e851636f213d646cbebbb8cdc8d8617
John Kohler <kohlerjohnp@gmail.com>
2017-10-04 10:48:38 -0400

    Parallel CI scripts (#1722)
    * Delete smoke test file
    
    * Separate out scripts
    
    * Don't run pronto in the background
    
    * fixup! Don't run pronto in the background
    
    * swap test order
    move codecov to mix tests
    remove extra compile step
    
    * Call mix crawl from smoke_test.sh

---
+++ semaphore/dialyzer.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+set -e
+# copy any pre-built PLTs to the right directory
+find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-dev.plt*" | xargs -I{} cp '{}' _build/dev
+
+export ERL_CRASH_DUMP=/dev/null
+mix dialyzer --plt
+
+# copy build PLTs back
+cp _build/dev/*_deps-dev.plt* $SEMAPHORE_CACHE_DIR
+
+mix dialyzer --halt-exit-status
--- semaphore/smoke_test.sh
+++ semaphore/smoke_test.sh
@@ -1,21 +1,2 @@
-#!/bin/bash
-set -ex
-if [[ "$1" ]]; then
-    DOMAIN=$1
-    HOST=$1
-    shift
-else
-    HOST=localhost
-    DOMAIN="$HOST:4001"
-fi
-OUTPUT_DIR=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
-OUTPUT_FILE=$OUTPUT_DIR/wget_output
-touch $OUTPUT_FILE
-if time wget -D $HOST --spider --delete-after --timeout 30 -nv -e robots=off -r "http://$DOMAIN/" -o $OUTPUT_FILE "$@"; then
-    rm -r $OUTPUT_DIR
-    echo PASSED!
-else
-    grep -v 'URL:' $OUTPUT_FILE
-    echo FAILED!
-    exit 1
-fi
+#/usr/bin/env bash
+mix escript.install --force https://s3.amazonaws.com/mbta-dotcom/crawler && $HOME/.mix/escripts/crawler https://dev.mbtace.com
