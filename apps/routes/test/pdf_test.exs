e4ba40596270745ae70f60cb92fb55acc87bb4ce
Sky Rose <sky@mbtace.com>
2017-10-17 16:11:45 -0400

    get route pdf using only id.

--- apps/routes/test/pdf_test.exs
+++ apps/routes/test/pdf_test.exs
@@ -5,28 +5,28 @@ defmodule Routes.PdfTest do
 
   describe "dated_urls/2" do
     test "given a date, returns upcoming schedule PDFs" do
-      route = %Route{id: "CR-Fairmount"}
+      route_id = "CR-Fairmount"
       expected = [
         {~D[2017-01-01], "/sites/default/files/route_pdfs/fairmount.pdf"},
         {~D[2017-05-22], "/sites/default/files/route_pdfs/Fairmont%20WEB%20052217%20V1.pdf"},
       ]
-      actual = dated_urls(route, ~D[2017-03-15])
+      actual = dated_urls(route_id, ~D[2017-03-15])
 
       assert actual == expected
     end
 
     test "filters out schedules once a new one is in effect" do
-      route = %Route{id: "CR-Worcester"}
+      route_id = "CR-Worcester"
       expected = [
         {~D[2017-05-22], "/sites/default/files/route_pdfs/Worcester%20WEB%20052217%20V1(1).pdf"}
       ]
-      assert dated_urls(route, ~D[2017-05-22]) == expected
-      assert dated_urls(route, ~D[2017-05-23]) == expected
+      assert dated_urls(route_id, ~D[2017-05-22]) == expected
+      assert dated_urls(route_id, ~D[2017-05-23]) == expected
     end
 
     test "returns an nil, nil if there isn't a matching route" do
-      route = %Route{id: "nonexistent"}
-      assert dated_urls(route, ~D[2017-01-01]) == []
+      route_id = "nonexistent"
+      assert dated_urls(route_id, ~D[2017-01-01]) == []
     end
   end
 
--- apps/site/web/controllers/schedule_v2/pdf.ex
+++ apps/site/web/controllers/schedule_v2/pdf.ex
@@ -1,22 +1,8 @@
 defmodule Site.ScheduleV2Controller.Pdf do
   use Site.Web, :controller
 
-  plug Site.Plugs.Route
-
-  def pdf(%Plug.Conn{assigns: %{route: route}} = conn, %{"date" => date_str}) do
-    with {:ok, date} <- Date.from_iso8601(date_str) do
-      pdf_from_route_and_date(conn, route, date)
-    else
-      _ -> render_404(conn)
-    end
-  end
-  def pdf(%Plug.Conn{assigns: %{route: route, date: date}} = conn, _params) do
-    pdf_from_route_and_date(conn, route, date)
-  end
-
-  @spec pdf_from_route_and_date(Plug.Conn.t, Routes.Route.t, Date.t) :: Plug.Conn.t
-  defp pdf_from_route_and_date(conn, route, date) do
-    case Routes.Pdf.dated_urls(route, date) do
+  def pdf(%Plug.Conn{assigns: %{date: date}} = conn, %{"route" => route_id}) do
+    case Routes.Pdf.dated_urls(route_id, date) do
       [] ->
         render_404(conn)
       [{_date, pdf_url} | _] ->
