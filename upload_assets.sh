a6fa6e490fdd9d123e1a202e893ca705196a1048
Gabe Durazo <gabe@durazo.us>
2017-10-18 07:55:21 -0500

    Only unzip assets from build

--- upload_assets.sh
+++ upload_assets.sh
@@ -5,7 +5,7 @@ VERSION=$(grep -o 'version: .*"' apps/site/mix.exs  | grep -E -o '([0-9]+\.)+[0-
 BUILD_ARTIFACT=site-build.zip
 TEMP_DIR=tmp_unzip
 
-unzip $BUILD_ARTIFACT -d $TEMP_DIR
+unzip $BUILD_ARTIFACT "lib/site-$VERSION/priv/static/*" -d $TEMP_DIR
 
 aws s3 sync $TEMP_DIR/lib/site-$VERSION/priv/static/css s3://mbta-dotcom/css --size-only
 aws s3 sync $TEMP_DIR/lib/site-$VERSION/priv/static/js s3://mbta-dotcom/js --size-only
