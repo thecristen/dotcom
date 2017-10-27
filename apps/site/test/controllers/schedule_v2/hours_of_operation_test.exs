59c35a429cbd9900da3a8a306bd09bfa7cbc3a64
Paul Swartz <paulswartz@gmail.com>
2017-10-27 14:49:26 -0400

    refactor template to take %HoursOfOperation{} directly

--- apps/site/test/controllers/schedule_v2/hours_of_operation_test.exs
+++ apps/site/test/controllers/schedule_v2/hours_of_operation_test.exs
@@ -4,6 +4,7 @@ defmodule Site.ScheduleV2Controller.HoursOfOperationTest do
   test "if route is nil, assigns nothing", %{conn: conn} do
     conn = conn
     |> assign(:route, nil)
+    |> assign(:date, Util.service_date())
     |> Site.ScheduleV2Controller.HoursOfOperation.call([])
 
     refute Map.has_key?(conn.assigns, :hours_of_operation)
@@ -12,29 +13,25 @@ defmodule Site.ScheduleV2Controller.HoursOfOperationTest do
   test "assigns week, saturday, and sunday departures in both directions", %{conn: conn} do
     conn = conn
     |> assign(:route, %Routes.Route{id: "Red"})
+    |> assign(:date, Util.service_date())
     |> Site.ScheduleV2Controller.HoursOfOperation.call([])
 
-    assert conn.assigns.hours_of_operation[:week][0].first_departure.hour == 5
-    assert conn.assigns.hours_of_operation[:week][0].last_departure.hour == 1
-    assert conn.assigns.hours_of_operation[:week][1].first_departure.hour == 5
-    assert conn.assigns.hours_of_operation[:saturday][1].first_departure.hour == 5
-    assert conn.assigns.hours_of_operation[:sunday][1].first_departure.hour == 6
+    assert %Schedules.HoursOfOperation{} = conn.assigns.hours_of_operation
   end
 
   test "uses schedules for each Green line branch", %{conn: conn} do
     conn = conn
     |> assign(:route, GreenLine.green_line())
+    |> assign(:date, Util.service_date())
     |> Site.ScheduleV2Controller.HoursOfOperation.call([])
 
-    assert conn.assigns.hours_of_operation[:week][0].first_departure.hour == 5
-    assert conn.assigns.hours_of_operation[:week][0].last_departure.hour == 1
-    assert conn.assigns.hours_of_operation[:week][1].first_departure.hour == 4
-    assert conn.assigns.hours_of_operation[:week][1].last_departure.hour == 1
+    assert %Schedules.HoursOfOperation{} = conn.assigns.hours_of_operation
   end
 
   test "assigns nothing if there is no service", %{conn: conn} do
-    conn = %{conn | params: %{"route" => "Teal"}}
+    conn = conn
     |> assign(:route, %Routes.Route{id: "Teal"})
+    |> assign(:date, Util.service_date())
     |> Site.ScheduleV2Controller.HoursOfOperation.call([])
 
     refute Map.has_key?(conn.assigns, :hours_of_operation)
--- apps/site/web/controllers/schedule_v2/hours_of_operation.ex
+++ apps/site/web/controllers/schedule_v2/hours_of_operation.ex
@@ -10,8 +10,7 @@ defmodule Site.ScheduleV2Controller.HoursOfOperation do
   def call(%Plug.Conn{assigns: %{route: route}} = conn, _opts) when not is_nil(route) do
     route.id
     |> full_route_id
-    |> Schedules.Repo.hours_of_operation
-    |> restructure_result
+    |> Schedules.Repo.hours_of_operation(conn.assigns.date)
     |> assign_hours(conn)
   end
   def call(conn, _opts) do
@@ -25,33 +24,6 @@ defmodule Site.ScheduleV2Controller.HoursOfOperation do
     route_id
   end
 
-  def restructure_result(hours) when hours == %HoursOfOperation{} do
-    %{}
-  end
-  def restructure_result(hours) do
-    %{
-      week: restructure_departures(hours.week),
-      saturday: restructure_departures(hours.saturday),
-      sunday: restructure_departures(hours.sunday)
-    }
-  end
-
-  defp restructure_departures({:no_service, :no_service}) do
-    nil
-  end
-  defp restructure_departures({departures_0, departures_1}) do
-    map = if departures_0 == :no_service do
-      %{}
-    else
-      %{0 => departures_0}
-    end
-    if departures_1 == :no_service do
-      map
-    else
-      Map.put(map, 1, departures_1)
-    end
-  end
-
-  defp assign_hours(hours, conn) when hours == %{}, do: conn
+  defp assign_hours(hours, conn) when hours == %HoursOfOperation{}, do: conn
   defp assign_hours(hours, conn), do: assign(conn, :hours_of_operation, hours)
 end
