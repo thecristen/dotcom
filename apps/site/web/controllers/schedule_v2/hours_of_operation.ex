0940d5a01e8757eee049b1a86376b202beb1a0cf
Paul Swartz <paulswartz@gmail.com>
2017-10-27 14:26:03 -0400

    refactor Site.ScheduleV2Controller.HoursOfOperation
    Now it uses the `Schedules.Repo.hours_of_operation/1` call.

--- apps/site/web/controllers/schedule_v2/hours_of_operation.ex
+++ apps/site/web/controllers/schedule_v2/hours_of_operation.ex
@@ -1,75 +1,57 @@
 defmodule Site.ScheduleV2Controller.HoursOfOperation do
   @behaviour Plug
   import Plug.Conn, only: [assign: 3]
+  alias Schedules.HoursOfOperation
 
   @impl true
   def init([]), do: []
 
   @impl true
-  def call(%Plug.Conn{assigns: %{route: route}} = conn, opts) when not is_nil(route) do
-    conn.assigns.date
-    |> get_dates()
-    |> Enum.map(fn date -> do_call(date, conn, Keyword.get(opts, :schedules_fn, &Schedules.Repo.by_route_ids/2)) end)
-    |> Enum.flat_map(fn {key, task} ->
-      case Task.yield(task) do
-        nil ->
-          []
-        {:ok, []} ->
-          []
-        {:ok, schedules} ->
-          [{key, schedules}]
-      end
-    end)
-    |> Enum.into(%{})
+  def call(%Plug.Conn{assigns: %{route: route}} = conn, _opts) when not is_nil(route) do
+    route.id
+    |> full_route_id
+    |> Schedules.Repo.hours_of_operation
+    |> restructure_result
     |> assign_hours(conn)
   end
   def call(conn, _opts) do
     conn
   end
 
-  defp do_call({key, date}, conn, schedules_fn) do
-    {key, Task.async(fn -> get_hours(conn, date, schedules_fn) end)}
+  defp full_route_id("Green") do
+    GreenLine.branch_ids()
+  end
+  defp full_route_id(route_id) do
+    route_id
+  end
+
+  def restructure_result(hours) when hours == %HoursOfOperation{} do
+    %{}
+  end
+  def restructure_result(hours) do
+    %{
+      week: restructure_departures(hours.week),
+      saturday: restructure_departures(hours.saturday),
+      sunday: restructure_departures(hours.sunday)
+    }
+  end
+
+  defp restructure_departures({:no_service, :no_service}) do
+    nil
+  end
+  defp restructure_departures({departures_0, departures_1}) do
+    map = if departures_0 == :no_service do
+      %{}
+    else
+      %{0 => departures_0}
+    end
+    if departures_1 == :no_service do
+      map
+    else
+      Map.put(map, 1, departures_1)
+    end
   end
 
   defp assign_hours(hours, conn) when hours == %{}, do: conn
   defp assign_hours(hours, conn), do: assign(conn, :hours_of_operation, hours)
-
-  defp get_hours(%Plug.Conn{assigns: %{route: %Routes.Route{id: "Green"}}}, date, schedules_fn) do
-    do_get_hours(GreenLine.branch_ids(), date, schedules_fn)
-  end
-  defp get_hours(%Plug.Conn{assigns: %{route: route}}, date, schedules_fn) do
-    do_get_hours([route.id], date, schedules_fn)
-  end
-
-  defp do_get_hours(route_ids, date, schedules_fn) do
-    with schedules when is_list(schedules) and schedules != [] <-
-      lookup_schedules(route_ids, date, schedules_fn) do
-
-      {inbound, outbound} = Enum.split_with(schedules, & &1.trip.direction_id == 1)
-      %{
-        1 => Schedules.Departures.first_and_last_departures(inbound),
-        0 => Schedules.Departures.first_and_last_departures(outbound)
-      }
-    end
-  end
-
-  defp lookup_schedules(route_ids, date, schedules_fn) do
-    route_ids
-    |> Task.async_stream(
-      &schedules_fn.([&1], date: date, stop_sequences: ~w(first last)s),
-      on_timeout: :kill)
-    |> Enum.flat_map(&extract_schedules/1)
-  end
-
-  defp extract_schedules({:ok, {:error, _}}), do: []
-  defp extract_schedules({:ok, schedules}), do: schedules
-  defp extract_schedules({:exit, _}), do: []
-
-  defp get_dates(date) do
-    %{
-      :week => Timex.end_of_week(date, 2),
-      :saturday => Timex.end_of_week(date, 7),
-      :sunday => Timex.end_of_week(date, 1)
-    }
-  end
 end
