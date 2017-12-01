dc0bfae538288dd74283353415a5b87aab27875e
Adamo Maisano <atwixtor@gmail.com>
2017-12-01 12:29:00 -0500

    Adds addtl coverage + no robots on "/node/x" paths

--- apps/content/test/cms/http_client_test.exs
+++ apps/content/test/cms/http_client_test.exs
@@ -4,6 +4,15 @@ defmodule Content.CMS.HTTPClientTest do
   import Content.CMS.HTTPClient
   alias Content.ExternalRequest
 
+  describe "preview/1" do
+    test "uses alternate path with timeout options" do
+      with_mock ExternalRequest, [process: fn(_method, _path, _body, _params) -> {:ok, []} end] do
+        preview("6")
+        assert called ExternalRequest.process(:get, "/api/revisions/6", "", [params: [_format: "json"], timeout: 30_000, recv_timeout: 30_000])
+      end
+    end
+  end
+  
   describe "view/2" do
     test "makes a get request with format: json params" do
       with_mock ExternalRequest, [process: fn(_method, _path, _body, _params) -> {:ok, []} end] do
