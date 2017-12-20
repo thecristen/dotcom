82ec5d3979a36f312d9cf1e82926cdd0636ec6b7
Adamo Maisano <atwixtor@gmail.com>
2017-12-20 17:54:48 -0500

    credo/pronto fix

--- apps/util/lib/breadcrumb_html.ex
+++ apps/util/lib/breadcrumb_html.ex
@@ -80,7 +80,7 @@ defmodule Util.BreadcrumbHTML do
     end
   end
 
-  def check_preview(%{query_params: %{"preview" => nil, "vid" => _}}, path = "/" <> _internal) do
+  def check_preview(%{query_params: %{"preview" => nil, "vid" => _}}, "/" <> _internal = path) do
     path <> "?preview&vid=latest"
   end
   def check_preview(_conn, path), do: path
