82257fa0c02716417a6d7c5c2cd837ee2295388c
Bill Peregoy <bill@peregoy.org>
2018-01-10 10:51:44 -0500

    Fix broken tests due to Wollaston station removal (#1971)

--- apps/site/test/site_web/controllers/schedule_v2/line_test.exs
+++ apps/site/test/site_web/controllers/schedule_v2/line_test.exs
@@ -131,7 +131,7 @@ defmodule SiteWeb.ScheduleV2Controller.LineTest do
         |> build_stop_list(0)
         |> Enum.map(fn {branches, stop} -> {branches, stop.id} end)
 
-      for {id, idx} <- [{"place-alfcl", 0}, {"place-jfk", 12}, {"place-brntn", 17}, {"place-asmnl", 21}] do
+      for {id, idx} <- [{"place-alfcl", 0}, {"place-jfk", 12}, {"place-brntn", 16}, {"place-asmnl", -1}] do
         assert stops |> Enum.at(idx) |> elem(1) == id
       end
     end
@@ -168,7 +168,7 @@ defmodule SiteWeb.ScheduleV2Controller.LineTest do
         |> build_stop_list(1)
         |> Enum.map(fn {branches, stop} -> {branches, stop.id} end)
 
-      for {id, idx} <- [{"place-alfcl", 21}, {"place-jfk", 9}, {"place-brntn", 4}, {"place-asmnl", 0}] do
+      for {id, idx} <- [{"place-alfcl", -1}, {"place-jfk", -13}, {"place-brntn", -17}, {"place-asmnl", 0}] do
         assert stops |> Enum.at(idx) |> elem(1) == id
       end
     end
