849b3ec4b31d680da83c160c3da63de226734d95
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-02-06 11:23:21 -0500

    handle empty results and blur event

--- apps/site/assets/js/search-bar.js
+++ apps/site/assets/js/search-bar.js
@@ -1,21 +1,54 @@
 import Sifter from 'sifter';
 
 export function setupSearch() {
-  const searchBar = document.getElementById("search-bar");
-  const buttons = document.getElementById("search-bar__results").children;
-  Array.from(buttons).forEach(btn => btn.classList.add("js-search-bar__result", "c-search-bar__result"));
-  const data = Array.from(buttons).map(el => { return {name: el.getAttribute("data-name")} })
-
-  searchBar.addEventListener("keyup", showResults(data));
+  document.addEventListener("turbolinks:load", doSetupSearch, {passive: true});
 }
 
-function showResults(data) {
-  return (ev) => {
-    document.getElementById("search-bar__results").classList
-      .remove("c-search-bar__results--hidden");
-    Array.from(document.getElementsByClassName("js-search-bar__result"))
-      .forEach(btn => btn.classList.add("c-search-bar__result--hidden"));
-    siftStops(data).forEach(addStopToResults);
+function doSetupSearch() {
+  if (document.getElementById("search-bar")) {
+    addButtonClasses();
+
+    const searchBar = document.getElementById("search-bar");
+    searchBar.addEventListener("keyup", showResults);
+    searchBar.addEventListener("blur", hideResults);
+  }
+}
+
+function buttonList() {
+  return Array.from(document.getElementsByClassName("js-search-bar__result"));
+}
+
+function addButtonClasses() {
+  const cls = [
+    "js-search-bar__result",
+    "c-search-bar__result",
+    "c-search-bar__result--hidden"
+  ];
+  Array.from(document.getElementById("search-bar__results").children)
+    .filter(el => el.href && el.href != "")
+    .forEach(btn => btn.classList.add(...cls));
+}
+
+function showResults() {
+  const data = buttonList().map(el => { return {name: el.getAttribute("data-name")} })
+  document.getElementById("search-bar__results").classList
+    .remove("c-search-bar__results--hidden");
+  Array.from(document.getElementsByClassName("js-search-bar__result"))
+    .forEach(btn => btn.classList.add("c-search-bar__result--hidden"));
+  const matches = siftStops(data)
+  if (matches.length > 0) {
+    document.getElementById("search-bar__empty").style.display = "none";
+    matches.forEach(addStopToResults);
+  } else {
+    document.getElementById("search-bar__empty").style.display = "block";
+  }
+}
+
+function hideResults(ev) {
+  if (ev.relatedTarget && ev.relatedTarget.classList.contains("js-search-bar__result")) {
+    return;
+  } else {
+    document.getElementById("search-bar__results").classList.add("c-search-bar__results--hidden");
   }
 }
 
@@ -30,7 +63,7 @@ export function siftStops(data) {
   const value = document.getElementById("search-bar").value
   return sifter.search(value, {
     fields: ['name'],
-    sort: [{field: 'name', direction: 'asc'}],
+    sort: [{field: 'name'}],
     limit: 10
   }).items.map(({id: id}) => data[id]);
 }
