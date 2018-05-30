313c9dd261f3ba873a6700b6c05cda382497e3a4
Bill Peregoy <bill@peregoy.org>
2018-05-30 10:00:06 -0400

    Testing | Update pronto on semaphore (#2232)

--- semaphore/pronto.sh
+++ semaphore/pronto.sh
@@ -1,22 +1,10 @@
 #/usr/bin/env bash
 set -e
 
-gem install pronto -v 0.7.1
-gem install pronto-eslint -v 0.7.0
-gem install pronto-scss -v 0.7.0
-
-# install custom pronto-credo
-if test -d $SEMAPHORE_CACHE_DIR/gems/pronto-credo; then
-    pushd $SEMAPHORE_CACHE_DIR/gems/pronto-credo
-else
-    pushd $SEMAPHORE_CACHE_DIR/gems
-    git clone https://github.com/paulswartz/pronto-credo.git
-    cd pronto-credo
-fi
-git checkout no-full-path
-gem build pronto-credo.gemspec
-gem install pronto-credo*.gem
-popd
+gem install pronto -v 0.9.5
+gem install pronto-eslint -v 0.9.1
+gem install pronto-scss -v 0.9.1
+gem install pronto-credo -v 0.0.8
 
 # run pronto
 MIX_ENV=test pronto run -f github github_status -c origin/master
