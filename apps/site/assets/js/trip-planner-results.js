589f8bf9c21c5ab6457050af0ad1c07cf1e5d896
Meaghan Harty <meag.harty@gmail.com>
2018-09-04 14:54:36 -0400

    Fixup print in firefox (#2443)

--- apps/site/assets/js/trip-planner-results.js
+++ apps/site/assets/js/trip-planner-results.js
@@ -13,27 +13,6 @@ export class TripPlannerResults {
     $("[data-planner-body]").on("show.bs.collapse", this.toggleIcon);
     $("[data-planner-body]").on("shown.bs.collapse", this.resetMapBounds);
     $(".itinerary-alert-toggle").on("click", this.toggleAlertDropdownText);
-    if (navigator.userAgent.search("Firefox") > 0) {
-      // We only want to load map images if they're actually being // used, to avoid spending money unnecessarily.
-      // Normally, that's accomplished by using background-image: url(); however, Firefox hides background images by
-      // default in printouts. This is a hack to load the static map image on Firefox only when javascript is enabled
-      // and the user has requested to print the page. The image is only visible under the @media print query, so
-      // it does not need to be removed after printing.
-      window.addEventListener("beforeprint", this.firefoxPrintStaticMap);
-    } else if (navigator.userAgent.search("CasperJS") === 0) {
-      // All other browsers load background images as expected when printing, so we set the background image url
-      // and remove the unnecessary image tag. Background images are only loaded when their element becomes visible,
-      // so the image will not be loaded unless the user activates the Print media query.
-      //
-      // Note that we also skip this when running in backstop as this was breaking backstop rendering with CasperJS
-      Array.from(document.getElementsByClassName("map-static")).map(div => {
-        div.setAttribute(
-          "style",
-          `background-image: url(${div.getAttribute("data-static-url")})`
-        );
-        return div.setAttribute("data-static-url", null);
-      });
-    }
   }
 
   bind() {
@@ -147,19 +126,6 @@ export class TripPlannerResults {
 
     if (this.maps[id]) {
       this.maps[id].panToBounds();
-    }
-  }
-
-  firefoxPrintStaticMap() {
-    const expanded = Array.from(
-      document.getElementsByClassName("trip-plan-itinerary-body")
-    ).find(el => el.classList.contains("in"));
-    if (expanded) {
-      const container = document.getElementById(`${expanded.id}-map-static`);
-      const img = document.createElement("img");
-      img.src = container.getAttribute("data-static-url");
-      img.classList.add("map-print");
-      container.appendChild(img);
     }
   }
 }
