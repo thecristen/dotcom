4c77e9319c37955e8e167e6fbed2316d091ad36d
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-09-10 15:30:32 -0400

    Refactor mode button grid styles (#2462)
    * refactor mode buttons
    
    * IE fixes
    
    * add view tests
    
    * fix button widths in IE
    
    * update padding & margins
    
    * subway 50% at sm
    
    * use base font for view all link
    
    * remove Lines/Routes text from headers
    
    * make alert icons blue
    
    * fixup! fix button widths in IE
    
    * update backstop
    
    * class*=

--- apps/site/lib/site_web/views/mode_view.ex
+++ apps/site/lib/site_web/views/mode_view.ex
@@ -1,6 +1,7 @@
 defmodule SiteWeb.ModeView do
   use SiteWeb, :view
   alias SiteWeb.PartialView.SvgIconWithCircle
+  alias Routes.Route
 
   def get_route_group(:commuter_rail = route_type, route_groups) do
     # Note that we do not sort the commuter rail routes by name as we
@@ -24,5 +25,98 @@ defmodule SiteWeb.ModeView do
   end
   def fares_note(_mode) do
       ""
+  end
+
+  @doc """
+  Builds the header tag for a mode group. Adds a "view all" link for bus.
+  """
+  @spec mode_group_header(atom, String.t, boolean) :: Phoenix.HTML.Safe.t
+  def mode_group_header(mode, href, is_homepage?) do
+    is_homepage?
+    |> mode_group_header_tag()
+    |> content_tag(mode_group_header_content(mode, href), class: "m-mode__header")
+  end
+
+  @spec mode_group_header_tag(boolean) :: :h2 | :h3
+  defp mode_group_header_tag(is_homepage?)
+  defp mode_group_header_tag(true), do: :h3
+  defp mode_group_header_tag(false), do: :h2
+
+  @spec mode_group_header_content(atom, String.t) :: [Phoenix.HTML.Safe.t]
+  defp mode_group_header_content(mode, href) do
+    [
+      link([
+        svg_icon_with_circle(%SvgIconWithCircle{icon: mode, aria_hidden?: true}),
+        " ",
+        Routes.Route.type_name(mode)
+      ], to: href, class: "m-mode__name"),
+      view_all_link(mode, href)
+    ]
+  end
+
+  @spec view_all_link(atom, String.t) :: [Phoenix.HTML.Safe.t]
+  defp view_all_link(:bus, href) do
+    [
+      link(["View all bus routes ", fa("arrow-right")], to: href, class: "m-mode__view-all")
+    ]
+  end
+  defp view_all_link(_, _) do
+    []
+  end
+
+  @spec mode_button_path(atom, Plug.Conn.t) :: String.t
+  def mode_button_path(:the_ride, %Plug.Conn{} = conn) do
+    cms_static_page_path(conn, "/accessibility/the-ride")
+  end
+  def mode_button_path(%Route{} = route, %Plug.Conn{} = conn) do
+    schedule_path(conn, :show, route)
+  end
+
+  @doc """
+  Returns the value to add as a modifier for the .c-mode-button class.
+  """
+  @spec mode_button_class_modifier(atom | Route.t) :: String.t
+  def mode_button_class_modifier(:the_ride) do
+    "the-ride"
+  end
+  def mode_button_class_modifier(%Route{} = route) do
+    route_to_class(route)
+  end
+
+  @doc """
+  Used to determine if the mode icon should be rendered on a mode button.
+  The Ride icon is never shown. Subway icons are always rendered.
+  Other modes rely on the :show_icon? boolean assign.
+  """
+  @spec show_icon?(atom | Route.t, boolean) :: boolean
+  def show_icon?(:the_ride, _) do
+    false
+  end
+  def show_icon?(%Route{type: type}, _) when type in [0, 1] do
+    true
+  end
+  def show_icon?(_, bool) when bool in [true, false] do
+    bool
+  end
+
+  @spec mode_button_text(atom | Route.t) :: String.t
+  def mode_button_text(:the_ride) do
+    "MBTA Paratransit Program"
+  end
+  def mode_button_text(%Route{name: name}) do
+    name
+  end
+
+  # Returns true if there is a non-notice alert for the given route on `date`
+  @spec has_alert?(Route.t | :the_ride, [Alerts.Alert.t], DateTime.t | nil) :: boolean
+  def has_alert?(:the_ride, _, _) do
+    false
+  end
+  def has_alert?(%Route{} = route, alerts, date) do
+    date = date || Util.now()
+    entity = %Alerts.InformedEntity{route_type: route.type, route: route.id}
+    Enum.any?(alerts, fn alert -> not Alerts.Alert.is_notice?(alert, date) &&
+                                  Alerts.Match.match([alert], entity, date) == [alert]
+                      end)
   end
 end
--- apps/site/test/site_web/views/mode_view_test.exs
+++ apps/site/test/site_web/views/mode_view_test.exs
@@ -1,14 +1,101 @@
 defmodule SiteWeb.ModeViewTest do
   use ExUnit.Case, async: true
 
-  import SiteWeb.ModeView
+  alias SiteWeb.ModeView
+  alias Routes.Route
+  alias Alerts.Alert
   import Phoenix.HTML, only: [safe_to_string: 1]
 
   describe "fares_note/1" do
     test "Commuter Rail has note" do
-      commuter_note = fares_note("Commuter Rail")
+      commuter_note = ModeView.fares_note("Commuter Rail")
       refute safe_to_string(commuter_note) == ""
-      assert fares_note("Ferry") == ""
+      assert ModeView.fares_note("Ferry") == ""
     end
+  end
+
+  describe "mode_group_header/3" do
+    test "renders an h2 if is_homepage? == false" do
+      assert {tag, _, _} =
+        :commuter_rail
+        |> ModeView.mode_group_header("/schedules/commuter-rail", false)
+        |> safe_to_string()
+        |> Floki.parse()
+      assert tag == "h2"
+    end
+
+    test "renders an h3 if is_homepage? == true" do
+      assert {tag, _, _} =
+        :commuter_rail
+        |> ModeView.mode_group_header("/schedules/commuter-rail", true)
+        |> safe_to_string()
+        |> Floki.parse()
+      assert tag == "h3"
+    end
+
+    test "sets name and link for mode" do
+      modes = [
+        {:bus, "Bus", "/schedules/bus"},
+        {:commuter_rail, "Commuter Rail", "/schedules/commuter-rail"},
+        {:subway, "Subway", "/schedules/subway"},
+        {:ferry, "Ferry", "/schedules/ferry"},
+        {:the_ride, "The RIDE", "/accessibility/the-ride"},
+      ]
+      for {mode, text, href} <- modes do
+        header =
+          mode
+          |> ModeView.mode_group_header(href, false)
+          |> safe_to_string()
+
+        assert header
+               |> Floki.find(".m-mode__name")
+               |> Floki.text()
+               |> String.trim() == text
+        assert header
+               |> Floki.find(".m-mode__name")
+               |> Floki.attribute("href") == [href]
+
+        view_all = Floki.find(header, ".m-mode__view-all")
+
+        if mode === :bus do
+          assert Floki.attribute(view_all, "href") == [href]
+          assert Floki.text(view_all) == "View all bus routes "
+        else
+          assert view_all == []
+        end
+      end
+    end
+  end
+
+  describe "has_alert?/3" do
+    test "returns true if route has an alert" do
+      now = Util.now()
+      entity = %Alerts.InformedEntity{route_type: 0, route: "Pink"}
+      alert = Alert.new(
+        active_period: [{Timex.shift(now, hours: -3), Timex.shift(now, hours: 3)}],
+        effect: :delay,
+        informed_entity: [entity]
+      )
+
+      assert Alerts.Alert.is_notice?(alert, now) == false
+      assert Alerts.Match.match([alert], entity, now) == [alert]
+
+      assert ModeView.has_alert?(%Route{id: "Pink", type: 0}, [alert], now) == true
+    end
+  end
+
+  test "returns false if route does not have an alert" do
+      now = Util.now()
+      entity = %Alerts.InformedEntity{route_type: 0, route: "Pink"}
+      alert = Alert.new(
+        active_period: [{Timex.shift(now, days: 5), Timex.shift(now, days: 9)}],
+        effect: :service_change,
+        informed_entity: [entity]
+      )
+
+      assert Alerts.Alert.is_notice?(alert, now) == true
+      assert Alerts.Match.match([alert], entity, now) == []
+
+      assert ModeView.has_alert?(%Route{id: "Pink", type: 0}, [alert], now) == false
   end
 end
