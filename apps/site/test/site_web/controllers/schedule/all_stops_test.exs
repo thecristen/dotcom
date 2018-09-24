a58e55dd363e181f75ab4019334797baf9955c3f
Meaghan Harty <meag.harty@gmail.com>
2018-09-24 12:37:02 -0700

    Fixup AllStopsTest (#2504)

--- apps/site/test/site_web/controllers/schedule/all_stops_test.exs
+++ apps/site/test/site_web/controllers/schedule/all_stops_test.exs
@@ -4,15 +4,30 @@ defmodule SiteWeb.ScheduleController.AllStopsTest do
 
   @moduletag :external
 
-  test "deduplicates green line stops", %{conn: conn} do
+  test "gets all Blue line stops", %{conn: conn} do
     conn = conn
-    |> assign(:date, nil)
+    |> assign(:date, Util.service_date())
     |> assign(:direction_id, 1)
-    |> assign(:route, %Routes.Route{id: "Green"})
+    |> assign(:route, %Routes.Route{id: "Blue"})
     |> AllStops.call([])
 
     all_stops = conn.assigns[:all_stops]
 
+    assert length(all_stops) > 0
     assert length(all_stops) == length(Enum.uniq_by(all_stops, &(&1.id)))
+  end
+
+  test "gets all Red line stops and adds wollaston", %{conn: conn} do
+    conn = conn
+    |> assign(:date, Util.service_date())
+    |> assign(:direction_id, 1)
+    |> assign(:route, %Routes.Route{id: "Red"})
+    |> AllStops.call([])
+
+    all_stops = conn.assigns[:all_stops]
+
+    assert length(all_stops) > 0
+    assert length(all_stops) == length(Enum.uniq_by(all_stops, &(&1.id)))
+    assert Enum.find(all_stops, fn s -> s.id == "place-wstn" end)
   end
 end
