7c42976d1b58fbc7c871c0df7e9b20d13bb338b6
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-10-22 19:45:43 -0400

    don't override wiremock proxy (#2570)

--- apps/trip_plan/config/prod.exs
+++ apps/trip_plan/config/prod.exs
@@ -1,5 +1,1 @@
 use Mix.Config
-
-config :trip_plan, OpenTripPlanner,
-  wiremock_proxy: "false"
-
--- apps/v3_api/config/prod.exs
+++ apps/v3_api/config/prod.exs
@@ -2,5 +2,4 @@ use Mix.Config
 
 config :v3_api,
   default_timeout: 5_000,
-  wiremock_proxy: "false",
   cache_size: 200_000
