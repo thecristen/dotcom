6f7a32876820a0ca1390e92e0e2c8d69f9c80c1a
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-10-30 09:54:04 -0400

    run wallaby in separate CI process (#2600)

--- semaphore/mix_test.sh
+++ semaphore/mix_test.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 set -e
-MIX_ENV=test mix coveralls.json -u
+MIX_ENV=test mix coveralls.json -u --exclude=wallaby
 
 # submit coverage report to codecov
 bash <(curl -s https://codecov.io/bash) -t $CODECOV_UPLOAD_TOKEN
---
+++ semaphore/wallaby.sh
@@ -0,0 +1,4 @@
+#/usr/bin/env bash
+set -e
+MIX_ENV=test mix test --only=wallaby
+
