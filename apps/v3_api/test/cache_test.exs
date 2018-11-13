6a524366e93072b36238b4a5f950ca1efebb2979
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-11-13 16:18:17 -0500

    use unique names for cache tests (#2634)

--- apps/v3_api/test/cache_test.exs
+++ apps/v3_api/test/cache_test.exs
@@ -3,7 +3,6 @@ defmodule V3Api.CacheTest do
   import V3Api.Cache
   alias HTTPoison.Response
 
-  @name __MODULE__
   @url "/url"
   @params []
   @last_modified "Fri, 06 Jul 2018 14:03:30 GMT"
@@ -17,64 +16,66 @@ defmodule V3Api.CacheTest do
   }
 
   setup do
-    {:ok, _pid} = start_link(name: @name, size: 2)
-    :ok
+    num = Enum.random(1..500)
+    name = String.to_atom("#{__MODULE__}#{num}")
+    {:ok, _pid} = start_link(name: name, size: 2)
+    {:ok, name: name}
   end
 
   describe "cache_response/3" do
-    test "304 response: returns a previously cached response" do
-      _ = cache_response(@name, @url, @params, @response)
+    test "304 response: returns a previously cached response", %{name: name} do
+      _ = cache_response(name, @url, @params, @response)
       not_modified = %{@response | status_code: 304}
-      assert {:ok, @response} == cache_response(@name, @url, @params, not_modified)
+      assert {:ok, @response} == cache_response(name, @url, @params, not_modified)
     end
 
-    test "200 response: returns the same response" do
-      assert {:ok, @response} == cache_response(@name, @url, @params, @response)
+    test "200 response: returns the same response", %{name: name} do
+      assert {:ok, @response} == cache_response(name, @url, @params, @response)
     end
 
-    test "400 response: returns the same response" do
+    test "400 response: returns the same response", %{name: name} do
       response = %{@response | status_code: 400}
-      assert {:ok, response} == cache_response(@name, @url, @params, response)
+      assert {:ok, response} == cache_response(name, @url, @params, response)
     end
 
-    test "404 response: returns the same response" do
+    test "404 response: returns the same response", %{name: name} do
       response = %{@response | status_code: 404}
-      assert {:ok, response} == cache_response(@name, @url, @params, response)
+      assert {:ok, response} == cache_response(name, @url, @params, response)
     end
 
-    test "500 response: returns the same response if not cached" do
+    test "500 response: returns the same response if not cached", %{name: name} do
       response = %{@response | status_code: 500}
-      assert {:ok, response} == cache_response(@name, @url, @params, response)
+      assert {:ok, response} == cache_response(name, @url, @params, response)
     end
 
-    test "500 response: returns a cached response if available" do
-      _ = cache_response(@name, @url, @params, @response)
+    test "500 response: returns a cached response if available", %{name: name} do
+      _ = cache_response(name, @url, @params, @response)
       response = %{@response | status_code: 500}
-      assert {:ok, @response} == cache_response(@name, @url, @params, response)
+      assert {:ok, @response} == cache_response(name, @url, @params, response)
     end
   end
 
   describe "cache_headers/2" do
-    test "returns an empty list if there's nothing cached" do
-      assert cache_headers(@name, @url, @params) == []
+    test "returns an empty list if there's nothing cached", %{name: name} do
+      assert cache_headers(name, @url, @params) == []
     end
 
-    test "once a response is cached, returns the last-modified header" do
-      _ = cache_response(@name, @url, @params, @response)
-      assert cache_headers(@name, @url, @params) == [{"if-modified-since", @last_modified}]
+    test "once a response is cached, returns the last-modified header", %{name: name} do
+      _ = cache_response(name, @url, @params, @response)
+      assert cache_headers(name, @url, @params) == [{"if-modified-since", @last_modified}]
     end
   end
 
   describe "expire!/0" do
-    test "removes the least-recently-used items to get the cache down to the right size" do
-      _ = cache_response(@name, "one", @params, @response)
-      _ = cache_response(@name, "expire", @params, @response)
-      _ = cache_response(@name, "also expire", @params, @response)
-      _ = cache_response(@name, "one", @params, %{@response | status_code: 304})
-      expire!(@name)
-      assert cache_headers(@name, "expire", @params) == []
-      assert cache_headers(@name, "also expire", @params) == []
-      refute cache_headers(@name, "one", @params) == []
+    test "removes the least-recently-used items to get the cache down to the right size", %{name: name} do
+      _ = cache_response(name, "one", @params, @response)
+      _ = cache_response(name, "expire", @params, @response)
+      _ = cache_response(name, "also expire", @params, @response)
+      _ = cache_response(name, "one", @params, %{@response | status_code: 304})
+      expire!(name)
+      assert cache_headers(name, "expire", @params) == []
+      assert cache_headers(name, "also expire", @params) == []
+      refute cache_headers(name, "one", @params) == []
     end
   end
 
