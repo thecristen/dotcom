691014adc633ecbb44ff3e1a1d37b8642ae85f09
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-11-12 19:37:42 -0500

    fix flaky CacheSupervisor test (#2632)

--- apps/site/test/site/lib/green_line/supervisor_test.exs
+++ apps/site/test/site/lib/green_line/supervisor_test.exs
@@ -1,5 +1,6 @@
 defmodule Site.GreenLine.CacheSupervisorTest do
   use ExUnit.Case
+  alias Site.GreenLine.DateAgent
 
   import Site.GreenLine.CacheSupervisor
 
@@ -9,9 +10,11 @@ defmodule Site.GreenLine.CacheSupervisorTest do
   end
 
   test "can start a child and retrieve it" do
-    date = Util.service_date
-    pid = lookup(date)
-    Site.GreenLine.DateAgent.stop(pid)
+    date = Util.service_date()
+    case lookup(date) do
+      nil -> :ok
+      pid -> DateAgent.stop(pid)
+    end
     assert lookup(date) == nil
 
     assert {:ok, pid} = start_child(date)
