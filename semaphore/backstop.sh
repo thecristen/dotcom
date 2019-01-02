64b8063399bbfd185bf0dd603275831b05a12112
Kate Hedgpeth <katehedgpeth@gmail.com>
2019-01-02 11:49:57 -0500

    re-enable backstop (#2749)
    * remove old wiremock data
    * update backstop trip planner params
    * update wiremock data
    * update backstop images
    * require backstop in semaphore

--- semaphore/backstop.sh
+++ semaphore/backstop.sh
@@ -39,5 +39,5 @@ else
   aws s3 cp $FILENAME s3://mbta-semaphore/$FILENAME
   LINK=`aws s3 presign s3://mbta-semaphore/$FILENAME --expires-in 1800`
   echo "Backstop report located at $LINK, available for 30 minutes."
-  exit 0
+  exit 1
 fi
