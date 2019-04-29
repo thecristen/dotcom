a81af1251be2a4eab3a5e937a331add032ffd300
Meaghan Harty <meaghan@mbtace.com>
2019-04-29 16:55:59 -0400

    Add polyfill for nextElementSibling in ie11 (#3088)

--- apps/site/assets/js/app.js
+++ apps/site/assets/js/app.js
@@ -265,6 +265,29 @@ if (!Array.prototype.find) {
   });
 }
 
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/NonDocumentTypeChildNode/nextElementSibling/nextElementSibling.md
+(function(arr) {
+  arr.forEach(function(item) {
+    if (item.hasOwnProperty("nextElementSibling")) {
+      return;
+    }
+    Object.defineProperty(item, "nextElementSibling", {
+      configurable: true,
+      enumerable: true,
+      get: function() {
+        var el = this;
+        while ((el = el.nextSibling)) {
+          if (el.nodeType === 1) {
+            return el;
+          }
+        }
+        return null;
+      },
+      set: undefined
+    });
+  });
+})([Element.prototype, CharacterData.prototype]);
+
 if ("outerHTML" in SVGElement.prototype) {
   Object.defineProperty(SVGElement.prototype, "outerHTML", {
     get() {
