6fb704274878dc2c399c8a0db8e677b2e0d5811d
Meaghan Harty <meaghan@mbtace.com>
2019-05-22 11:58:45 -0400

    Disable sentry for JS (#3153)

--- apps/site/assets/ts/schedule-entry.ts
+++ apps/site/assets/ts/schedule-entry.ts
@@ -1,14 +1,3 @@
-import * as Sentry from "@sentry/browser";
 import schedule from "./schedule/schedule-loader";
-
-/* eslint-disable no-undef */
-// @ts-ignore webpack constants
-if (process.env.NODE_ENV === "production" && SENTRY_DSN) {
-  Sentry.init({
-    // @ts-ignore webpack constants
-    dsn: SENTRY_DSN
-  });
-}
-/* eslint-enable no-undef */
 
 schedule();
--- apps/site/assets/ts/stop-entry.ts
+++ apps/site/assets/ts/stop-entry.ts
@@ -1,16 +1,5 @@
-import * as Sentry from "@sentry/browser";
 import googleMapsLoaded from "../js/google-maps-loaded";
 import stop from "./stop/stop-loader";
-
-/* eslint-disable no-undef */
-// @ts-ignore webpack constants
-if (process.env.NODE_ENV === "production" && SENTRY_DSN) {
-  Sentry.init({
-    // @ts-ignore webpack constants
-    dsn: SENTRY_DSN
-  });
-}
-/* eslint-enable no-undef */
 
 googleMapsLoaded();
 stop();
