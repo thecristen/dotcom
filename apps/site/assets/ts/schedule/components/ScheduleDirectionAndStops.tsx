bb41d4eadcf15ef556c0b5ffbfd28526d7ce496c
Phil Darnowsky <pdarnowsky@mbta.com>
2019-09-26 10:26:28 -0400

    Make static stop list work serverside

--- apps/site/assets/ts/schedule/components/ScheduleDirectionAndStops.tsx
+++ apps/site/assets/ts/schedule/components/ScheduleDirectionAndStops.tsx
@@ -1,21 +1,26 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import ScheduleDirection from "./ScheduleDirection";
 import { SchedulePageData } from "./__schedule";
+import { DirectionId } from "../../__v3api";
 
 export interface SchedulePageDataWithStopListContent extends SchedulePageData {
   stop_list_html: string;
 }
 
-const ScheduleDirectionAndStops = (schedulePageData: SchedulePageDataWithStopListContent): ReactElement<HTMLElement> => (
-  <>
-    <ScheduleDirection
-      directionId={schedulePageData.direction_id}
-      route={schedulePageData.route}
-      shapesById={schedulePageData.shape_map}
-      routePatternsByDirection={schedulePageData.route_patterns}
-    />
-    <div dangerouslySetInnerHTML={{__html: schedulePageData.stop_list_html}} />
-  </>
-)
+const ScheduleDirectionAndStops = (schedulePageData: SchedulePageDataWithStopListContent): ReactElement<HTMLElement> => {
+  const [stopListHtml, updateStopListHtml] = useState(schedulePageData.stop_list_html);
+
+  return (
+    <>
+      <ScheduleDirection
+        directionId={schedulePageData.direction_id}
+        route={schedulePageData.route}
+        shapesById={schedulePageData.shape_map}
+        routePatternsByDirection={schedulePageData.route_patterns}
+      />
+      <div dangerouslySetInnerHTML={{__html: stopListHtml}} />
+    </>
+  );
+}
 
 export default ScheduleDirectionAndStops;
